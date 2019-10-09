185ebf37a56774d96716db299676db27100fedf4
Alan Grgic <alan.grgic@code42.com>
2019-10-09 11:59:20 -0500

    initial structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="c42seceventcli",
+    version="0.1.0",
+    description="Tools for integrating Code42 Exfiltration Detection features with SIEMs",
+    packages=find_packages(include=["c42seceventcli", "c42seceventcli.*"]),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    install_requires=["c42secevents"],
+    license="MIT",
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        "dev": [
+            "pre-commit==1.18.3",
+            "pytest==4.6.5",
+            "pytest-mock==1.10.4",
+        ]
+    },
+)
