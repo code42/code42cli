150937c555530f4eb9b107e7e23b1ab80d74ffb0
Juliya Smith <juliya.smith@code42.com>
2019-10-09 12:19:51 -0500

    Set up cli

--- aed_cursor_store.py
+++ aed_cursor_store.py
@@ -1,72 +1,32 @@
-from os import path
-import sqlite3
-
+from common import SecurityEventCursorStore
 
 _INSERTION_TIMESTAMP_FIELD_NAME = u"insertionTimestamp"
-_TABLE_NAME = "siem_checkpoint"
-_PRIMARY_KEY_COLUMN_NAME = "cursor_id"
-_PRIMARY_KEY = 1
 
 
-class AEDCursorStore(object):
-    def __init__(self, db_file_path=None):
-        # type: (str) -> None
-        if db_file_path is None:
-            script_path = path.dirname(path.realpath(__file__))
-            db_file_path = "{0}/{1}.db".format(script_path, _TABLE_NAME)
-
-        self._connection = sqlite3.connect(db_file_path)
-        if self._is_empty():
-            self._init_table()
-
+class AEDCursorStore(SecurityEventCursorStore):
     @property
     def insertion_timestamp(self):
-        query = "SELECT {0} FROM {1}} WHERE {2}=?".format(
-            _INSERTION_TIMESTAMP_FIELD_NAME, _TABLE_NAME, _PRIMARY_KEY_COLUMN_NAME
-        )
-        with self._connection as conn:
-            cursor = conn.cursor()
-            cursor.execute(query, (_PRIMARY_KEY,))
-            rows = cursor.fetchone()
-            if rows:
-                return rows[0]
+        rows = self._get(_INSERTION_TIMESTAMP_FIELD_NAME, self._primary_key)
+        if rows:
+            return rows[0]
 
     @insertion_timestamp.setter
     def insertion_timestamp(self, value):
-        query = "UPDATE {0}} SET {1}=? WHERE {2}=?".format(
-            _TABLE_NAME,
-            _INSERTION_TIMESTAMP_FIELD_NAME,
-            _PRIMARY_KEY_COLUMN_NAME
+        self._set(
+            column_name=_INSERTION_TIMESTAMP_FIELD_NAME,
+            new_value=value,
+            primary_key=self._primary_key,
         )
-        with self._connection as conn:
-            conn.execute(query, (value, _PRIMARY_KEY,))
 
-    def reset(self):
-        self._drop_table()
-        self._init_table()
+    @property
+    def _primary_key(self):
+        return "1"
 
     def _init_table(self):
-        columns = "{0}, {1}".format(_PRIMARY_KEY_COLUMN_NAME, _INSERTION_TIMESTAMP_FIELD_NAME)
-        create_table_query = "CREATE TABLE {0} ({1})".format(_TABLE_NAME, columns)
-        insert_query = "INSERT INTO {0} VALUES(?, null)".format(_TABLE_NAME)
+        insertion_ts_col = _INSERTION_TIMESTAMP_FIELD_NAME
+        columns = "{0}, {1}".format(self._primary_key_column_name, insertion_ts_col)
+        create_table_query = "CREATE TABLE {0} ({1})".format(self._table_name, columns)
+        insert_query = "INSERT INTO {0} VALUES(?, null)".format(self._table_name)
         with self._connection as conn:
             conn.execute(create_table_query)
-            conn.execute(insert_query, (_PRIMARY_KEY,))
-
-    def _drop_table(self):
-        drop_query = "DROP TABLE {0}".format(_TABLE_NAME)
-        with self._connection as conn:
-            conn.execute(drop_query)
-
-    def _is_empty(self):
-        table_count_query = """
-            SELECT COUNT(name)
-            FROM sqlite_master
-            WHERE type='table' AND name=?
-        """
-        with self._connection as conn:
-            cursor = conn.cursor()
-            cursor.execute(table_count_query, (_TABLE_NAME,))
-            query_result = cursor.fetchone()
-            if query_result:
-                return int(query_result[0]) <= 0
+            conn.execute(insert_query, (self._primary_key,))
---
+++ aed_event_extraction_cli.py
@@ -0,0 +1,109 @@
+from argparse import SUPPRESS
+from argparse import ArgumentParser
+import urllib3
+
+from py42.sdk import SDK
+from py42 import settings
+from c42secevents.extractors import extract_aed_events
+from c42secevents.common import FileEventHandlers
+from aed_cursor_store import AEDCursorStore
+
+
+def main():
+    parser = _get_arg_parser()
+    args = parser.parse_args()
+
+    if args.c42_ignore_ssl_errors:
+        _ignore_ssl_errors()
+
+    sdk = _create_sdk(
+        address=args.c42_authority_url,
+        username=args.c42_username,
+        password=args.c42_password
+    )
+
+    _set_handlers()
+    extract_aed_events(sdk, 1564694804)
+
+
+def _get_arg_parser():
+    parser = ArgumentParser(add_help=False)
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
+    required.add_argument(
+        "-s",
+        "--server",
+        dest="c42_authority_url",
+        action="store",
+        help="The full scheme, url and port of the Code42 server.",
+        required=True,
+    )
+    required.add_argument(
+        "-u",
+        "--username",
+        action="store",
+        dest="c42_username",
+        help="The username of the Code42 API user.",
+        required=True,
+    )
+    required.add_argument(
+        "-p",
+        "--password",
+        action="store",
+        dest="c42_password",
+        help="The password of the Code42 API user.",
+        required=True,
+    )
+    required.add_argument(
+        "-b",
+        "--begin",
+        action="store",
+        dest="c42_begin_date",
+        help="The beginning of the date range in which to look for events, "
+        "in YYYY-MM-DD format OR a number (number of minutes ago).",
+        required=True,
+    )
+    optional.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=SUPPRESS,
+        help="Show this help message and exit",
+    )
+    optional.add_argument(
+        "-i",
+        "--ignore-ssl-errors",
+        action="store_true",
+        dest="c42_ignore_ssl_errors",
+        help="Set to ignore ssl errors.",
+    )
+    return parser
+
+
+def _ignore_ssl_errors():
+    settings.verify_ssl_certs = False
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def _create_sdk(address, username, password):
+    return SDK.create_using_local_account(
+        host_address=address, username=username, password=password
+    )
+
+
+def _set_handlers():
+    store = AEDCursorStore()
+
+    def record_timestamp(timestamp):
+        store.insertion_timestamp = timestamp
+
+    def get_timestamp():
+        return store.insertion_timestamp
+
+    FileEventHandlers.record_cursor_position = record_timestamp
+    FileEventHandlers.get_cursor_position = get_timestamp
+    # TODO: FileEventHandlers.handle_response = HOOK FOR LOGGING
+
+
+if __name__ == "__main__":
+    main()
