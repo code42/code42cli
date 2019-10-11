a540001628b182c4311bea57cea71b45af8c8219
Juliya Smith <juliya.smith@code42.com>
2019-10-11 10:20:59 -0500

    Use class vars

--- c42seceventcli/aed/aed_cursor_store.py
+++ c42seceventcli/aed/aed_cursor_store.py
@@ -23,9 +23,9 @@ class AEDCursorStore(SecurityEventCursorStore):
         return 1
 
     def _init_table(self):
-        columns = "{0}, {1}".format(self._primary_key_column_name, _INSERTION_TIMESTAMP_FIELD_NAME)
-        create_table_query = "CREATE TABLE {0} ({1})".format(self._table_name, columns)
-        insert_query = "INSERT INTO {0} VALUES(?, null)".format(self._table_name)
+        columns = "{0}, {1}".format(self._PRIMARY_KEY_COLUMN_NAME, _INSERTION_TIMESTAMP_FIELD_NAME)
+        create_table_query = "CREATE TABLE {0} ({1})".format(self._TABLE_NAME, columns)
+        insert_query = "INSERT INTO {0} VALUES(?, null)".format(self._TABLE_NAME)
         with self._connection as conn:
             conn.execute(create_table_query)
             conn.execute(insert_query, (self._primary_key,))
--- c42seceventcli/common/cursor_store.py
+++ c42seceventcli/common/cursor_store.py
@@ -3,23 +3,18 @@ from os import path
 
 
 class SecurityEventCursorStore(object):
+    _TABLE_NAME = "siem_checkpoint"
+    _PRIMARY_KEY_COLUMN_NAME = "cursor_id"
+
     def __init__(self, db_file_path=None):
         # type: (str) -> None
         if db_file_path is None:
             script_path = path.dirname(path.realpath(__file__))
-            db_file_path = "{0}/{1}.db".format(script_path, self._table_name)
+            db_file_path = "{0}/{1}.db".format(script_path, self._TABLE_NAME)
 
         self._connection = sqlite3.connect(db_file_path)
         if self._is_empty():
             self._init_table()
-
-    @property
-    def _table_name(self):
-        return "siem_checkpoint"
-
-    @property
-    def _primary_key_column_name(self):
-        return "cursor_id"
 
     def reset(self):
         self._drop_table()
@@ -32,7 +27,7 @@ class SecurityEventCursorStore(object):
     def _get(self, columns, primary_key):
         # type: (str, any) -> list
         query = "SELECT {0} FROM {1} WHERE {2}=?"
-        query = query.format(columns, self._table_name, self._primary_key_column_name)
+        query = query.format(columns, self._TABLE_NAME, self._PRIMARY_KEY_COLUMN_NAME)
         with self._connection as conn:
             cursor = conn.cursor()
             cursor.execute(query, (primary_key,))
@@ -41,13 +36,13 @@ class SecurityEventCursorStore(object):
     def _set(self, column_name, new_value, primary_key):
         # type: (str, any, any) -> None
         query = "UPDATE {0} SET {1}=? WHERE {2} = ?".format(
-            self._table_name, column_name, self._primary_key_column_name
+            self._TABLE_NAME, column_name, self._PRIMARY_KEY_COLUMN_NAME
         )
         with self._connection as conn:
             conn.execute(query, (new_value, primary_key))
 
     def _drop_table(self):
-        drop_query = "DROP TABLE {0}".format(self._table_name)
+        drop_query = "DROP TABLE {0}".format(self._TABLE_NAME)
         with self._connection as conn:
             conn.execute(drop_query)
 
@@ -59,7 +54,7 @@ class SecurityEventCursorStore(object):
         """
         with self._connection as conn:
             cursor = conn.cursor()
-            cursor.execute(table_count_query, (self._table_name,))
+            cursor.execute(table_count_query, (self._TABLE_NAME,))
             query_result = cursor.fetchone()
             if query_result:
                 return int(query_result[0]) <= 0
