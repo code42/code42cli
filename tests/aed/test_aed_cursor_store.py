c300be5e15718acf50982ff084bc668a5506eac6
Juliya Smith <juliya.smith@code42.com>
2019-10-11 12:21:51 -0500

    Remove method for configuring mock store for clarity

--- tests/aed/test_aed_cursor_store.py
+++ tests/aed/test_aed_cursor_store.py
@@ -1,18 +1,18 @@
 from c42secevents.extractors import INSERTION_TIMESTAMP_FIELD_NAME
 from c42seceventcli.aed.aed_cursor_store import AEDCursorStore
-from ..conftest import configure_store_with_mock_sqlite, MOCK_TEST_DB_PATH
+from ..conftest import MOCK_TEST_DB_PATH
 
 
 class TestAEDCursorStore(object):
     def test_reset_calls_drop_table_query_first(self, mocker, sqlite_connection):
         store = AEDCursorStore(MOCK_TEST_DB_PATH)
         execute_function = mocker.MagicMock()
-        configure_store_with_mock_sqlite(
-            store=store,
-            mocker=mocker,
-            sqlite_connection=sqlite_connection,
-            execute_function=execute_function,
-        )
+        store._connection = sqlite_connection
+        with store._connection as conn:
+            mock_cursor = mocker.MagicMock()
+            mock_cursor.execute = execute_function
+            conn.cursor.return_value = mock_cursor
+            conn.execute = execute_function
         store.reset()
         actual = execute_function.call_args_list[0][0][0]
         expected = "DROP TABLE siem_checkpoint"
@@ -21,12 +21,13 @@ class TestAEDCursorStore(object):
     def test_reset_calls_create_table_query_second(self, mocker, sqlite_connection):
         store = AEDCursorStore(MOCK_TEST_DB_PATH)
         execute_function = mocker.MagicMock()
-        configure_store_with_mock_sqlite(
-            store=store,
-            mocker=mocker,
-            sqlite_connection=sqlite_connection,
-            execute_function=execute_function,
-        )
+        store._connection = sqlite_connection
+        with store._connection as conn:
+            mock_cursor = mocker.MagicMock()
+            mock_cursor.execute = execute_function
+            conn.cursor.return_value = mock_cursor
+            conn.execute = execute_function
+
         store.reset()
         actual = execute_function.call_args_list[1][0][0]
         expected = "CREATE TABLE siem_checkpoint (cursor_id, insertionTimestamp)"
@@ -35,12 +36,12 @@ class TestAEDCursorStore(object):
     def test_reset_calls_insert_query_third(self, mocker, sqlite_connection):
         store = AEDCursorStore(MOCK_TEST_DB_PATH)
         execute_function = mocker.MagicMock()
-        configure_store_with_mock_sqlite(
-            store=store,
-            mocker=mocker,
-            sqlite_connection=sqlite_connection,
-            execute_function=execute_function,
-        )
+        store._connection = sqlite_connection
+        with store._connection as conn:
+            mock_cursor = mocker.MagicMock()
+            mock_cursor.execute = execute_function
+            conn.cursor.return_value = mock_cursor
+            conn.execute = execute_function
         store.reset()
         actual = execute_function.call_args_list[2][0][0]
         expected = "INSERT INTO siem_checkpoint VALUES(?, null)"
@@ -51,12 +52,12 @@ class TestAEDCursorStore(object):
     ):
         store = AEDCursorStore(MOCK_TEST_DB_PATH)
         execute_function = mocker.MagicMock()
-        configure_store_with_mock_sqlite(
-            store=store,
-            mocker=mocker,
-            sqlite_connection=sqlite_connection,
-            execute_function=execute_function,
-        )
+        store._connection = sqlite_connection
+        with store._connection as conn:
+            mock_cursor = mocker.MagicMock()
+            mock_cursor.execute = execute_function
+            conn.cursor.return_value = mock_cursor
+            conn.execute = execute_function
         expected_query = "SELECT {0} FROM siem_checkpoint WHERE cursor_id=?".format(
             INSERTION_TIMESTAMP_FIELD_NAME
         )
@@ -68,12 +69,12 @@ class TestAEDCursorStore(object):
     ):
         store = AEDCursorStore(MOCK_TEST_DB_PATH)
         execute_function = mocker.MagicMock()
-        configure_store_with_mock_sqlite(
-            store=store,
-            mocker=mocker,
-            sqlite_connection=sqlite_connection,
-            execute_function=execute_function,
-        )
+        store._connection = sqlite_connection
+        with store._connection as conn:
+            mock_cursor = mocker.MagicMock()
+            mock_cursor.execute = execute_function
+            conn.cursor.return_value = mock_cursor
+            conn.execute = execute_function
         expected_query = "UPDATE siem_checkpoint SET {0}=? WHERE cursor_id = ?".format(
             INSERTION_TIMESTAMP_FIELD_NAME
         )
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,26 +8,5 @@ MOCK_TEST_DB_PATH = "test_path.db"
 
 
 @pytest.fixture
-def test_store(mocker, sqlite_connection, execute_function):
-    store = SecurityEventCursorStore(MOCK_TEST_DB_PATH)
-    store._connection = sqlite_connection
-    return configure_store_with_mock_sqlite(
-        store=store,
-        mocker=mocker,
-        sqlite_connection=sqlite_connection,
-        execute_function=execute_function,
-    )
-
-
-@pytest.fixture
 def sqlite_connection(mocker):
     return mocker.patch("sqlite3.connect")
-
-
-def configure_store_with_mock_sqlite(store, mocker, sqlite_connection, execute_function):
-    store._connection = sqlite_connection
-    with store._connection as conn:
-        mock_cursor = mocker.MagicMock()
-        mock_cursor.execute = execute_function
-        conn.cursor.return_value = mock_cursor
-        conn.execute = execute_function
