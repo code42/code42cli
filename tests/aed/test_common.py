b416e021bbafe991e36c52fc4956acc19efb2675
Juliya Smith <juliya.smith@code42.com>
2019-10-15 17:00:14 -0500

    Test config parser

--- tests/aed/test_common.py
+++ tests/aed/test_common.py
@@ -4,17 +4,30 @@ from datetime import datetime
 from c42seceventcli.common.common import SecurityEventConfigParser
 
 
+_DUMMY_KEY = "Key"
+
+
 @pytest.fixture
-def mock_config_parser(mocker):
-    mock_parser = mocker.patch("configparser.ConfigParser")
+def mock_config_get_function(mocker):
+    return mocker.patch("configparser.ConfigParser.get")
 
 
 class TestSecurityEventConfigParser(object):
-    def test_parse_server_returns_expected_value(self, mock_config_parser):
-        # expected = "SERVER"
-        # mock_config_parser.get.return_value = expected
-        # parser = SecurityEventConfigParser("test")
-        # actual = parser.p
-        assert True
+    def test_get_returns_expected_value(self, mock_config_get_function):
+        expected = "Value"
+        mock_config_get_function.return_value = expected
+        parser = SecurityEventConfigParser("test")
+        actual = parser.get(_DUMMY_KEY)
+        assert actual == expected
 
+    def test_get_bool_returns_expected_bool(self, mock_config_get_function):
+        mock_config_get_function.return_value = True
+        parser = SecurityEventConfigParser("test")
+        assert parser.get_bool(_DUMMY_KEY)
 
+    def test_get_int_returns_expected_int(self, mock_config_get_function):
+        expected = 42
+        mock_config_get_function.return_value = 42
+        parser = SecurityEventConfigParser("test")
+        actual = parser.get_int(_DUMMY_KEY)
+        assert actual == expected
