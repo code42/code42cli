d10488c3de31da3a9318a90ff499f06b2b76a5fd
Juliya Smith <juliya.smith@code42.com>
2019-10-15 17:07:00 -0500

    Test parse timestamp

--- tests/aed/test_common.py
+++ tests/aed/test_common.py
@@ -1,7 +1,7 @@
 import pytest
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from c42seceventcli.common.common import SecurityEventConfigParser
+from c42seceventcli.common.common import SecurityEventConfigParser, parse_timestamp
 
 
 _DUMMY_KEY = "Key"
@@ -31,3 +31,25 @@ class TestSecurityEventConfigParser(object):
         parser = SecurityEventConfigParser("test")
         actual = parser.get_int(_DUMMY_KEY)
         assert actual == expected
+
+
+def test_parse_timestamp_when_given_date_format_returns_expected_timestamp():
+    date_str = "2019-10-01"
+    date = datetime.strptime(date_str, "%Y-%m-%d")
+    expected = (date - date.utcfromtimestamp(0)).total_seconds()
+    actual = parse_timestamp(date_str)
+    assert actual == expected
+
+
+def test_parse_timestamp_when_given_minutes_ago_format_returns_expected_timestamp():
+    minutes_ago = 1000
+    now = datetime.utcnow()
+    time = now - timedelta(minutes=minutes_ago)
+    expected = (time - datetime.utcfromtimestamp(0)).total_seconds()
+    actual = parse_timestamp("1000")
+    assert pytest.approx(actual, expected)
+
+
+def test_parse_timestamp_when_given_bad_string_throws_value_error():
+    with pytest.raises(ValueError):
+        parse_timestamp("BAD!")
