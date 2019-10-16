f7772a359d9d079a320a294bb6707197bae9957f
Juliya Smith <juliya.smith@code42.com>
2019-10-16 13:09:02 -0500

    Test get logger

--- tests/aed/test_common.py
+++ tests/aed/test_common.py
@@ -1,7 +1,10 @@
 import pytest
 from datetime import datetime, timedelta
+from logging import StreamHandler, FileHandler
 
-from c42seceventcli.common.common import SecurityEventConfigParser, parse_timestamp
+from c42secevents.logging.handlers import NoPrioritySysLogHandler
+
+from c42seceventcli.common.common import SecurityEventConfigParser, parse_timestamp, get_logger
 
 
 _DUMMY_KEY = "Key"
@@ -15,6 +18,11 @@ def mock_config_get_function(mocker):
 @pytest.fixture
 def mock_config_get_bool_function(mocker):
     return mocker.patch("configparser.ConfigParser.getboolean")
+
+
+@pytest.fixture
+def mock_get_logger(mocker):
+    return mocker.patch("c42seceventcli.common.common.getLogger")
 
 
 class TestSecurityEventConfigParser(object):
@@ -58,3 +66,26 @@ def test_parse_timestamp_when_given_minutes_ago_format_returns_expected_timestam
 def test_parse_timestamp_when_given_bad_string_throws_value_error():
     with pytest.raises(ValueError):
         parse_timestamp("BAD!")
+
+
+def test_get_logger_when_destination_type_is_stdout_adds_stream_handler_to_logger(mock_get_logger):
+    logger = get_logger(None, "Somewhere", "stdout")
+    actual = type(logger.addHandler.call_args[0][0])
+    expected = StreamHandler
+    assert actual == expected
+
+
+def test_get_logger_when_destination_type_is_file_adds_file_handler_to_logger(mock_get_logger):
+    logger = get_logger(None, "Somewhere", "file")
+    actual = type(logger.addHandler.call_args[0][0])
+    expected = FileHandler
+    assert actual == expected
+
+
+def test_get_logger_when_destination_type_is_syslog_adds_no_priority_sys_log_handler_to_logger(mocker, mock_get_logger):
+    mock_handler_init = mocker.patch("c42secevents.logging.handlers.NoPrioritySysLogHandler.__init__")
+    mock_handler_init.return_value = None
+    logger = get_logger(None, "Somewhere", "syslog")
+    actual = type(logger.addHandler.call_args[0][0])
+    expected = NoPrioritySysLogHandler
+    assert actual == expected
