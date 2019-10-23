35e4bc8c9077e7e6ebc3a713961e34b4430a465f
Juliya Smith <juliya.smith@code42.com>
2019-10-23 11:55:27 -0500

    Test get user proj path

--- tests/common/test_common.py
+++ tests/common/test_common.py
@@ -1,4 +1,5 @@
 import pytest
+from os import path
 from datetime import datetime, timedelta
 from logging import StreamHandler, FileHandler
 from logging.handlers import RotatingFileHandler
@@ -111,6 +112,58 @@ def mock_getpass_function(mocker):
 @pytest.fixture
 def mock_get_input(mocker):
     return mocker.patch("c42seceventcli.common.common._get_input")
+
+
+@pytest.fixture
+def path_patches(mocker, mock_user_expansion, mock_dir_maker, mock_path_existence):
+    mock = mocker.MagicMock()
+    mock.expand_user = mock_user_expansion
+    mock.make_dirs = mock_dir_maker
+    mock.path_exists = mock_path_existence
+    return mock
+
+
+@pytest.fixture
+def mock_user_expansion(mocker):
+    return mocker.patch("os.path.expanduser")
+
+
+@pytest.fixture
+def mock_dir_maker(mocker):
+    return mocker.patch("c42seceventcli.common.common.makedirs")
+
+
+@pytest.fixture
+def mock_path_existence(mocker):
+    return mocker.patch("os.path.exists")
+
+
+def test_get_user_project_path_returns_expected_path(path_patches):
+    expected_home = "/PATH/"
+    expected_subdir = "SUBDIR"
+    path_patches.expand_user.return_value = expected_home
+    expected = path.join(expected_home, ".c42seceventcli", expected_subdir)
+    actual = get_user_project_path(expected_subdir)
+    assert actual == expected
+
+
+def test_get_user_project_path_calls_make_dirs_when_path_does_not_exist(path_patches):
+    expected_home = "/PATH/"
+    expected_subdir = "SUBDIR"
+    path_patches.expand_user.return_value = expected_home
+    expected_path = path.join(expected_home, ".c42seceventcli", expected_subdir)
+    path_patches.path_exists.return_value = False
+    get_user_project_path(expected_subdir)
+    path_patches.make_dirs.assert_called_once_with(expected_path)
+
+
+def test_get_user_project_path_does_not_call_make_durs_when_path_exists(path_patches):
+    expected_home = "/PATH/"
+    expected_subdir = "SUBDIR"
+    path_patches.expand_user.return_value = expected_home
+    path_patches.path_exists.return_value = True
+    get_user_project_path(expected_subdir)
+    assert not path_patches.make_dirs.call_count
 
 
 def test_get_config_args_when_read_returns_empty_list_raises_io_error(mocker):
