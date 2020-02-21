c1e1bc1c3ef70cbc2dd9031eed35c578fa71bc6a
Juliya Smith <juliya.smith@code42.com>
2020-02-21 19:23:42 +0000

    Test main.py

---
+++ tests/test_main.py
@@ -0,0 +1,68 @@
+import pytest
+
+from c42sec.main import main
+
+
+@pytest.fixture
+def profile(mocker):
+    return mocker.patch("c42sec.subcommands.profile.init")
+
+
+@pytest.fixture
+def cursor_clearer(mocker):
+    return mocker.patch("c42sec.subcommands.clear_cursor.init")
+
+
+@pytest.fixture
+def printer(mocker):
+    return mocker.patch("c42sec.subcommands.print_out.init")
+
+
+@pytest.fixture
+def writer(mocker):
+    return mocker.patch("c42sec.subcommands.write_to.init")
+
+
+@pytest.fixture
+def sender(mocker):
+    return mocker.patch("c42sec.subcommands.send_to.init")
+
+
+@pytest.fixture
+def arg_parser(mocker):
+    return mocker.patch("argparse.ArgumentParser.parse_args")
+
+
+def test_main_inits_profile(profile, arg_parser):
+    main()
+    assert profile.call_count == 1
+
+
+def test_main_inits_clear_cursor(cursor_clearer, arg_parser):
+    main()
+    assert cursor_clearer.call_count == 1
+
+
+def test_main_inits_print(printer, arg_parser):
+    main()
+    assert printer.call_count == 1
+
+
+def test_main_inits_write_to(writer, arg_parser):
+    main()
+    assert writer.call_count == 1
+
+
+def test_main_inits_send_to(sender, arg_parser):
+    main()
+    assert sender.call_count == 1
+
+
+def test_main_calls_subcommand_with_parsed_args(mocker, arg_parser, namespace):
+    arg_parser.return_value = namespace
+    namespace.format = "TEST"
+    namespace.exposure_types = ["EXPOSURE"]
+    namespace.func = mocker.MagicMock()
+    main()
+    actual_args = namespace.func.call_args[0][0]
+    assert actual_args == namespace
