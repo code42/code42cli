382b493d3e276f0dd983d56c1b6c47fb08d0d378
Juliya Smith <juliya.smith@code42.com>
2020-02-24 19:21:57 +0000

    Removed unnecessaries

--- code42cli/main.py
+++ code42cli/main.py
@@ -7,20 +7,7 @@ import code42cli.subcommands.securitydata.main as securitydata
 def main():
     code42_arg_parser = ArgumentParser()
     subcommand_parser = code42_arg_parser.add_subparsers()
-    _init_subcommands(subcommand_parser)
-    _call_subcommand(code42_arg_parser)
-
-
-def _init_subcommands(subcommand_parser):
     profile.init(subcommand_parser)
     securitydata.init_subcommand(subcommand_parser)
-
-
-def _call_subcommand(arg_parser):
-    """Call provided subcommand with args."""
-    args = arg_parser.parse_args()
+    args = code42_arg_parser.parse_args()
     args.func(args)
-
-
-if __name__ == "__main__":
-    main()
