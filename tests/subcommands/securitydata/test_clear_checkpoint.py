1a5f75fd95682cdb4fed3c787a2c5039cacc96f3
Juliya Smith <juliya.smith@code42.com>
2020-02-24 20:10:08 +0000

    Format

--- tests/subcommands/securitydata/test_clear_checkpoint.py
+++ tests/subcommands/securitydata/test_clear_checkpoint.py
@@ -5,7 +5,9 @@ from code42cli.subcommands.securitydata.clear_checkpoint import clear_checkpoint
 
 @pytest.fixture
 def cursor_store(mocker):
-    mock_init = mocker.patch("code42cli._internal.securitydata.cursor_store.AEDCursorStore.__init__")
+    mock_init = mocker.patch(
+        "code42cli._internal.securitydata.cursor_store.AEDCursorStore.__init__"
+    )
     mock_init.return_value = None
     mock = mocker.MagicMock()
     mock_new = mocker.patch("code42cli._internal.securitydata.cursor_store.AEDCursorStore.__new__")
