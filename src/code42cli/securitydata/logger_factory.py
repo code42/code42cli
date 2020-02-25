6b8dd22e0bace547e71f8eed2c351c0e8c68b6f2
Juliya Smith <juliya.smith@code42.com>
2020-02-25 19:45:58 +0000

    Shared logger has handlers func

--- src/code42cli/securitydata/logger_factory.py
+++ src/code42cli/securitydata/logger_factory.py
@@ -14,7 +14,7 @@ from code42cli.util import get_user_project_path
 
 def get_logger_for_stdout(output_format):
     logger = logging.getLogger("code42_stdout_{0}".format(output_format.lower()))
-    if len(logger.handlers) > 0:
+    if _logger_has_handlers(logger):
         return logger
 
     handler = logging.StreamHandler(sys.stdout)
@@ -23,7 +23,7 @@ def get_logger_for_stdout(output_format):
 
 def get_logger_for_file(filename, output_format):
     logger = logging.getLogger("code42_file_{0}".format(output_format.lower()))
-    if len(logger.handlers) > 0:
+    if _logger_has_handlers(logger):
         return logger
 
     handler = logging.FileHandler(filename, delay=True)
@@ -32,7 +32,7 @@ def get_logger_for_file(filename, output_format):
 
 def get_logger_for_server(hostname, protocol, output_format):
     logger = logging.getLogger("code42_syslog_{0}".format(output_format.lower()))
-    if len(logger.handlers) > 0:
+    if _logger_has_handlers(logger):
         return logger
 
     handler = NoPrioritySysLogHandler(hostname, protocol=protocol)
@@ -43,12 +43,16 @@ def get_error_logger():
     log_path = get_user_project_path("log")
     log_path = "{0}/code42_errors.log".format(log_path)
     logger = logging.getLogger("code42_error_logger")
-    if len(logger.handlers) > 0:
+    if _logger_has_handlers(logger):
         return logger
 
     formatter = logging.Formatter("%(asctime)s %(message)s")
     handler = RotatingFileHandler(log_path, maxBytes=250000000)
     return _apply_logger_dependencies(logger, handler, formatter)
+
+
+def _logger_has_handlers(logger):
+    return len(logger.handlers)
 
 
 def _init_logger(logger, handler, output_format):
