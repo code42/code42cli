4374a254f6fc3e2455686677653ebcfbf12a8d27
Juliya Smith <juliya.smith@code42.com>
2020-03-12 19:56:27 +0000

    Refactor password

--- src/code42cli/profile/password.py
+++ src/code42cli/profile/password.py
@@ -11,8 +11,7 @@ _ROOT_SERVICE_NAME = u"code42cli"
 
 def get_password(profile_name):
     """Gets your currently stored password for your profile."""
-    accessor = get_config_accessor()
-    profile = accessor.get_profile(profile_name)
+    profile = _get_profile(profile_name)
     service_name = _get_service_name(profile_name)
     username = _get_username(profile)
     password = keyring.get_password(service_name, username)
@@ -20,27 +19,27 @@ def get_password(profile_name):
 
 
 def set_password(profile_name, new_password):
-    accessor = get_config_accessor()
-    profile = accessor.get_profile(profile_name)
+    profile = _get_profile(profile_name)
     service_name = _get_service_name(profile_name)
     username = _get_username(profile)
     keyring.set_password(service_name, username, new_password)
+    print(u"'Code42 Password' updated.")
 
 
 def set_password_from_prompt(profile_name):
     """Prompts and sets your password for your profile."""
     password = getpass()
-    accessor = get_config_accessor()
-    profile = accessor.get_profile(profile_name)
-    service_name = _get_service_name(profile_name)
-    username = _get_username(profile)
-    keyring.set_password(service_name, username, password)
-    print(u"'Code42 Password' updated.")
+    set_password(profile_name, password)
     return password
 
 
 def get_password_from_prompt():
     return getpass()
+
+
+def _get_profile(profile_name):
+    accessor = get_config_accessor()
+    return accessor.get_profile(profile_name)
 
 
 def _get_service_name(profile_name):
