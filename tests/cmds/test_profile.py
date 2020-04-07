3aa417628ac59a1dc87da850bcf0130ebf6479b0
Alan Grgic <alan.grgic@code42.com>
2020-04-07 13:07:30 -0500

    pr feedback

--- tests/cmds/test_profile.py
+++ tests/cmds/test_profile.py
@@ -19,7 +19,7 @@ def user_disagreement(mocker):
 
 
 @pytest.fixture
-def mock_cliprofile_ns(mocker):
+def mock_cliprofile_namespace(mocker):
     return mocker.patch("code42cli.cmds.profile.cliprofile")
 
 
@@ -34,12 +34,12 @@ def mock_verify(mocker):
     return mocker.patch("code42cli.cmds.profile.validate_connection")
 
 
-def test_show_profile_outputs_profile_info(capsys, mock_cliprofile_ns, profile):
+def test_show_profile_outputs_profile_info(capsys, mock_cliprofile_namespace, profile):
     profile.name = "testname"
     profile.authority_url = "example.com"
     profile.username = "foo"
     profile.disable_ssl_errors = True
-    mock_cliprofile_ns.get_profile.return_value = profile
+    mock_cliprofile_namespace.get_profile.return_value = profile
     profilecmd.show_profile(profile)
     capture = capsys.readouterr()
     assert "testname" in capture.out
@@ -48,16 +48,18 @@ def test_show_profile_outputs_profile_info(capsys, mock_cliprofile_ns, profile):
     assert "A password is set" in capture.out
 
 
-def test_show_profile_when_password_set_outputs_password_note(capsys, mock_cliprofile_ns, profile):
-    mock_cliprofile_ns.get_profile.return_value = profile
-    mock_cliprofile_ns.get_stored_password.return_value = None
+def test_show_profile_when_password_set_outputs_password_note(
+    capsys, mock_cliprofile_namespace, profile
+):
+    mock_cliprofile_namespace.get_profile.return_value = profile
+    mock_cliprofile_namespace.get_stored_password.return_value = None
     profilecmd.show_profile(profile)
     capture = capsys.readouterr()
     assert "A password is set" not in capture.out
 
 
-def test_create_profile_if_profile_exists_exits(capsys, mock_cliprofile_ns):
-    mock_cliprofile_ns.profile_exists.return_value = True
+def test_create_profile_if_profile_exists_exits(capsys, mock_cliprofile_namespace):
+    mock_cliprofile_namespace.profile_exists.return_value = True
     success = True
     try:
         profilecmd.create_profile("foo", "bar", "baz", True)
@@ -69,74 +71,74 @@ def test_create_profile_if_profile_exists_exits(capsys, mock_cliprofile_ns):
 
 
 def test_create_profile_if_user_sets_password_is_created(
-    user_agreement, mock_verify, mock_cliprofile_ns
+    user_agreement, mock_verify, mock_cliprofile_namespace
 ):
-    mock_cliprofile_ns.profile_exists.return_value = False
+    mock_cliprofile_namespace.profile_exists.return_value = False
     profilecmd.create_profile("foo", "bar", "baz", True)
-    mock_cliprofile_ns.create_profile.assert_called_once_with("foo", "bar", "baz", True)
+    mock_cliprofile_namespace.create_profile.assert_called_once_with("foo", "bar", "baz", True)
 
 
 def test_create_profile_if_user_does_not_set_password_is_created(
-    user_disagreement, mock_verify, mock_cliprofile_ns
+    user_disagreement, mock_verify, mock_cliprofile_namespace
 ):
-    mock_cliprofile_ns.profile_exists.return_value = False
+    mock_cliprofile_namespace.profile_exists.return_value = False
     profilecmd.create_profile("foo", "bar", "baz", True)
-    mock_cliprofile_ns.create_profile.assert_called_once_with("foo", "bar", "baz", True)
+    mock_cliprofile_namespace.create_profile.assert_called_once_with("foo", "bar", "baz", True)
 
 
 def test_create_profile_if_credentials_invalid_password_not_saved(
-    user_agreement, mock_verify, mock_cliprofile_ns
+    user_agreement, mock_verify, mock_cliprofile_namespace
 ):
     mock_verify.return_value = False
-    mock_cliprofile_ns.profile_exists.return_value = False
+    mock_cliprofile_namespace.profile_exists.return_value = False
     success = False
     try:
         profilecmd.create_profile("foo", "bar", "baz", True)
     except SystemExit:
         success = True
-        assert not mock_cliprofile_ns.set_password.call_count
+        assert not mock_cliprofile_namespace.set_password.call_count
     assert success
 
 
 def test_create_profile_if_credentials_valid_password_saved(
-    mocker, user_agreement, mock_verify, mock_cliprofile_ns
+    mocker, user_agreement, mock_verify, mock_cliprofile_namespace
 ):
     mock_verify.return_value = True
-    mock_cliprofile_ns.profile_exists.return_value = False
+    mock_cliprofile_namespace.profile_exists.return_value = False
     profilecmd.create_profile("foo", "bar", "baz", True)
-    mock_cliprofile_ns.set_password.assert_called_once_with("newpassword", mocker.ANY)
+    mock_cliprofile_namespace.set_password.assert_called_once_with("newpassword", mocker.ANY)
 
 
 def test_prompt_for_password_reset_if_credentials_valid_password_saved(
-    mocker, user_agreement, mock_verify, mock_cliprofile_ns
+    mocker, user_agreement, mock_verify, mock_cliprofile_namespace
 ):
     mock_verify.return_value = True
-    mock_cliprofile_ns.profile_exists.return_value = False
+    mock_cliprofile_namespace.profile_exists.return_value = False
     profilecmd.prompt_for_password_reset()
-    mock_cliprofile_ns.set_password.assert_called_once_with("newpassword", mocker.ANY)
+    mock_cliprofile_namespace.set_password.assert_called_once_with("newpassword", mocker.ANY)
 
 
 def test_prompt_for_password_reset_if_credentials_invalid_password_not_saved(
-    user_agreement, mock_verify, mock_cliprofile_ns
+    user_agreement, mock_verify, mock_cliprofile_namespace
 ):
     mock_verify.return_value = False
-    mock_cliprofile_ns.profile_exists.return_value = False
+    mock_cliprofile_namespace.profile_exists.return_value = False
     success = False
     try:
         profilecmd.prompt_for_password_reset()
     except SystemExit:
         success = True
-        assert not mock_cliprofile_ns.set_password.call_count
+        assert not mock_cliprofile_namespace.set_password.call_count
     assert success
 
 
-def test_list_profiles(capsys, mock_cliprofile_ns):
+def test_list_profiles(capsys, mock_cliprofile_namespace):
     profiles = [
         create_mock_profile("one"),
         create_mock_profile("two"),
         create_mock_profile("three"),
     ]
-    mock_cliprofile_ns.get_all_profiles.return_value = profiles
+    mock_cliprofile_namespace.get_all_profiles.return_value = profiles
     profilecmd.list_profiles()
     capture = capsys.readouterr()
     assert "one" in capture.out
@@ -144,13 +146,15 @@ def test_list_profiles(capsys, mock_cliprofile_ns):
     assert "three" in capture.out
 
 
-def test_list_profiles_when_no_profiles_outputs_no_profiles_message(capsys, mock_cliprofile_ns):
-    mock_cliprofile_ns.get_all_profiles.return_value = []
+def test_list_profiles_when_no_profiles_outputs_no_profiles_message(
+    capsys, mock_cliprofile_namespace
+):
+    mock_cliprofile_namespace.get_all_profiles.return_value = []
     profilecmd.list_profiles()
     capture = capsys.readouterr()
     assert "No existing profile." in capture.out
 
 
-def test_use_profile(mock_cliprofile_ns, profile):
+def test_use_profile(mock_cliprofile_namespace, profile):
     profilecmd.use_profile(profile)
-    mock_cliprofile_ns.switch_default_profile.assert_called_once_with(profile)
+    mock_cliprofile_namespace.switch_default_profile.assert_called_once_with(profile)
