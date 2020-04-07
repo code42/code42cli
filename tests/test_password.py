90a685b085958c31659d863fe6ac097a175fff1c
Alan Grgic <alan.grgic@code42.com>
2020-04-07 14:08:22 -0500

    add fallback prompt tests

--- tests/test_password.py
+++ tests/test_password.py
@@ -15,9 +15,30 @@ def keyring_password_setter(mocker):
     return mocker.patch("keyring.set_password")
 
 
+@pytest.fixture(autouse=True)
+def get_keyring(mocker):
+    mock = mocker.patch("keyring.get_keyring")
+    mock.return_value.priority = 10
+    return mock
+
+
 @pytest.fixture
 def getpass_function(mocker):
     return mocker.patch("code42cli.password.getpass")
+
+
+@pytest.fixture
+def user_agreement(mocker):
+    mock = mocker.patch("code42cli.password.does_user_agree")
+    mock.return_value = True
+    return mocker
+
+
+@pytest.fixture
+def user_disagreement(mocker):
+    mock = mocker.patch("code42cli.password.does_user_agree")
+    mock.return_value = False
+    return mocker
 
 
 def test_get_stored_password_when_given_profile_name_gets_profile_for_that_name(
@@ -48,6 +69,30 @@ def test_set_password_uses_expected_service_name_username_and_password(
     keyring_password_setter.assert_called_once_with(
         expected_service_name, profile.username, "test_password"
     )
+
+
+def test_set_password_when_using_file_fallback_and_user_accepts_saves_password(
+    profile, keyring_password_setter, keyring_password_getter, get_keyring, user_agreement
+):
+    keyring_password_getter.return_value = "test_password"
+    profile.name = "profile_name"
+    profile.username = "test.username"
+    password.set_password(profile, "test_password")
+    expected_service_name = "code42cli::profile_name"
+    keyring_password_setter.assert_called_once_with(
+        expected_service_name, profile.username, "test_password"
+    )
+
+
+def test_set_password_when_using_file_fallback_and_user_rejects_does_not_saves_password(
+    profile, keyring_password_setter, get_keyring, user_disagreement
+):
+    get_keyring.return_value.priority = 0.5
+    keyring_password_getter.return_value = "test_password"
+    profile.name = "profile_name"
+    profile.username = "test.username"
+    password.set_password(profile, "test_password")
+    assert not keyring_password_setter.call_count
 
 
 def test_prompt_for_password_calls_getpass(getpass_function):
