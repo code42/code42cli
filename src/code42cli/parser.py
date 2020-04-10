2ac3845558c32d47973fe40f56397716716ff386
Juliya Smith <juliya.smith@code42.com>
2020-04-10 14:40:25 +0100

    Fix trailing white space

--- src/code42cli/parser.py
+++ src/code42cli/parser.py
@@ -4,10 +4,10 @@ from py42.__version__ import __version__ as py42version
 
 from code42cli.__version__ import __version__ as cliversion
 
-BANNER = u"""
- dP""b8  dP"Yb  8888b. 888888  dP88  oP"Yb.
-dP   `" dP   Yb 8I  Yb 88__   dP 88  "' dP'
-Yb      Yb   dP 8I  dY 88""  d888888   dP'
+BANNER = u""" 
+ dP""b8  dP"Yb  8888b. 888888  dP88  oP"Yb. 
+dP   `" dP   Yb 8I  Yb 88__   dP 88  "' dP' 
+Yb      Yb   dP 8I  dY 88""  d888888   dP'  
  YboodP  YbodP  8888Y" 888888    88  .d8888
 
 code42cli version {}, by Code42 Software.
