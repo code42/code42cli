87067efe06cf6b5c3833f943d201e0e9e2b3e963
Juliya Smith <juliya.smith@code42.com>
2020-04-13 20:21:43 +0100

    Simplfy async test

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,26 +1,16 @@
+import time
+
 from code42cli.worker import Worker
 
 
 class TestWorker(object):
     def test_is_async(self):
-        # This test starts a thread that waits for the main thread to add an element to list,
-        # then it adds an item to list right after. The fact that the new thread waits for the main
-        # thread proves it's existence and the fact that it does busy-waiting proves that it's
-        # parallel.
         worker = Worker(5)
-        test_limit = 100
         demo_ls = []
 
         def async_func():
-            i = 0
-            # Wait until the line below `do_async` adds an item to `demo_ls`.
-            while demo_ls == [] and i < test_limit:
-                i += 1
-
-            # This is to prevent infinite loops.
-            if i == test_limit:
-                assert False
-
+            # Wait so that the line under `do_async` happens first, proving that it's async
+            time.sleep(0.01)
             demo_ls.append(2)
 
         worker.do_async(async_func)
