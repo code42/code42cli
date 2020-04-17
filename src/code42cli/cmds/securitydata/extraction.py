3f138cc8600de8b747830202325ea561c6c7fc49
Tim Abramson <tim.abramson@code42.com>
2020-04-17 14:44:35 -0500

    Feature/magic time args (#37)
    * implement magic strings
    
    * updated changelog
    
    * put back u-prefixes
    
    * missed u
    
    * more u's
    
    * more u's
    
    * add magic string description to readme
    
    * - refactor duplicate logic in _parse_{min,max}_timestamp into _parse_timestamp
    - fix tests to use new DateArgumentException

--- src/code42cli/cmds/securitydata/extraction.py
+++ src/code42cli/cmds/securitydata/extraction.py
@@ -117,10 +117,10 @@ def _create_filters(args):
 
 def _get_event_timestamp_filter(begin_date, end_date):
     try:
-        begin_date = begin_date.strip().split() if begin_date else None
-        end_date = end_date.strip().split() if end_date else None
+        begin_date = begin_date.strip() if begin_date else None
+        end_date = end_date.strip() if end_date else None
         return date_helper.create_event_timestamp_filter(begin_date, end_date)
-    except ValueError as ex:
+    except date_helper.DateArgumentException as ex:
         print_error(str(ex))
         exit(1)
 
