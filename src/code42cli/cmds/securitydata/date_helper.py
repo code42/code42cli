d903a75abaf9e4a6c27c9146d2da8a843d49daed
Tim Abramson <tim.abramson@code42.com>
2020-04-16 19:19:33 -0500

    more u's

--- src/code42cli/cmds/securitydata/date_helper.py
+++ src/code42cli/cmds/securitydata/date_helper.py
@@ -5,7 +5,7 @@ from c42eventextractor.common import convert_datetime_to_timestamp
 from py42.sdk.queries.fileevents.filters.event_filter import EventTimestamp
 
 _MAX_LOOK_BACK_DAYS = 90
-_FORMAT_VALUE_ERROR_MESSAGE = "input must be a date in YYYY-MM-DD or YYYY-MM-DD HH:MM:SS format, or a short value in days, hours, or minutes (30d, 24h, 30m)"
+_FORMAT_VALUE_ERROR_MESSAGE = u"input must be a date in YYYY-MM-DD or YYYY-MM-DD HH:MM:SS format, or a short value in days, hours, or minutes (30d, 24h, 30m)"
 
 TIMESTAMP_REGEX = re.compile("(\d{4}-\d{2}-\d{2})\s*(.*)?")
 MAGIC_TIME_REGEX = re.compile("(\d+)([dhm])$")
@@ -66,7 +66,7 @@ def _parse_min_timestamp(begin_date_str):
         datetime.utcnow() - timedelta(days=_MAX_LOOK_BACK_DAYS)
     )
     if dt < boundary_date:
-        raise ValueError("'Begin date' must be within 90 days.")
+        raise ValueError(u"'Begin date' must be within 90 days.")
 
     return convert_datetime_to_timestamp(dt)
 
@@ -94,7 +94,7 @@ def _parse_max_timestamp(end_date_str):
 
 
 def _get_dt_from_date_time_pair(date, time):
-    date_format = "%Y-%m-%d %H:%M:%S"
+    date_format = u"%Y-%m-%d %H:%M:%S"
     time = time or u"00:00:00"
     date_string = u"{} {}".format(date, time)
     try:
@@ -114,7 +114,7 @@ def _get_dt_from_magic_time_pair(num, period):
     elif period == "m":
         dt = datetime.utcnow() - timedelta(minutes=num)
     else:
-        raise ValueError("Couldn't parse magic time string: {}{}".format(num, period))
+        raise ValueError(u"Couldn't parse magic time string: {}{}".format(num, period))
     return dt
 
 
@@ -122,7 +122,7 @@ def _verify_timestamp_order(min_timestamp, max_timestamp):
     if min_timestamp is None or max_timestamp is None:
         return
     if min_timestamp >= max_timestamp:
-        raise ValueError("Begin date cannot be after end date")
+        raise ValueError(u"Begin date cannot be after end date")
 
 
 def _round_datetime_to_day_start(dt):
