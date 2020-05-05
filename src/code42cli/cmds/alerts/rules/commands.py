759737472fa6585f79168e987f7889bd30a8e8c9
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-05-05 13:34:44 +0530

    Refactor: List, remove and bulk command to parameter changes

--- src/code42cli/cmds/alerts/rules/commands.py
+++ src/code42cli/cmds/alerts/rules/commands.py
@@ -1,11 +1,8 @@
 from code42cli.cmds.alerts.rules.user_rule import (
     add_user, 
-    remove_user, 
-    remove_all_users, 
-    get_by_rule_type,
-    get_all,
-    add_bulk_users,
-    remove_bulk_users,
+    remove_user,
+    get_rules,
+    bulk_update,
 )
 from code42cli.commands import Command
 
@@ -36,10 +33,13 @@ def _customize_list_arguments(argument_collection):
     rule_type.set_help(u"Type of rule, either of 'exfiltration', 'cloudshare', 'filetypemismatch'")
     rule_type.set_choices([u"exfiltration", u"cloudshare", u"filetypemismatch"])
     rule_id = argument_collection.arg_configs[u"rule_id"]
-    rule_id.set_help(u"Rule id of th rule")
+    rule_id.set_help(u"Observer rule id of th rule")
 
 
-def bulk_arguments(argument_collection):
+def _customize_bulk_arguments(argument_collection):
+    action = argument_collection.arg_configs[u"action"]
+    action.set_help("Type of update operation, either of 'add' or 'remove'")
+    action.set_choices([u"add", u"remove"])
     file_name = argument_collection.arg_configs[u"file_name"]
     file_name.set_help(u"CSV file name with relative path."
                        u"Format rule_id,user_id to update rule for specific user, "
@@ -57,53 +57,29 @@ def load_subcommands():
             arg_customizer=_customize_add_arguments
         )
 
-    add_bulk = Command(
-        u"add-bulk",
-        u"Update multiple alert rules to monitor user aliases against the Uid for the given rule id."
-        u"csv file format: rule_id, user_id",
-        u"{} {}".format(usage_prefix, u"add-bulk <filenamce>"),
-        handler=add_bulk_users,
-        arg_customizer=bulk_arguments
+    bulk = Command(
+        u"bulk",
+        u"Update multiple alert rules to monitor user aliases against the Code42 userUid "
+        u"and given rule id. CSV file format: rule_id, user_id",
+        u"{} {}".format(usage_prefix, u"bulk <action> <filename>"),
+        handler=bulk_update,
+        arg_customizer=_customize_bulk_arguments
     )
 
-    remove_one = Command(
+    remove = Command(
             u"remove-user",
             u"Update alert rule criteria to remove a user and all its aliases.",
-            u"{} {}".format(usage_prefix, u"remove-user <rule_id>  <user_id>"),
+            u"{} {}".format(usage_prefix, u"remove-user <rule_id>  --user-id UX"),
             handler=remove_user,
             arg_customizer=_customize_remove_arguments
         )
-
-    remove_all = Command(
-        u"remove-all",
-        u"Update alert rule criteria to remove all users and all its aliases from a rule.",
-        u"{} {}".format(usage_prefix, u"remove-all <rule_id>"),
-        handler=remove_all_users,
-        arg_customizer=_customize_remove_all_arguments
-    )
     
-    remove_bulk = Command(
-        u"remove-bulk",
-        u"Update multiple alert rule criteria to remove all users and all its aliases from a rule."
-        u"csv file format: rule_id",
-        u"{} {}".format(usage_prefix, u"remove-bulk <filename>"),
-        handler=remove_bulk_users,
-        arg_customizer=bulk_arguments
-    )
-
     list_rules = Command(
             u"list",
-            u"Fetch existing alert rules by rule -id",
-            u"{} {}".format(usage_prefix, u"list <rule_type>  <rule_id>"),
-            handler=get_by_rule_type,
+            u"Fetch existing alert rules, optionally fetch by by rule-id",
+            u"{} {}".format(usage_prefix, u"list --rule-type [type] --rule-id UX"),
+            handler=get_rules,
             arg_customizer=_customize_list_arguments
         )
 
-    list_all_rules = Command(
-        u"list-all",
-        u"Fetch all existing alert rules.",
-        u"{} {}".format(usage_prefix, u"list-all"),
-        handler=get_all,
-    )
-
-    return [add, add_bulk, remove_one, remove_all, remove_bulk, list_rules, list_all_rules]
+    return [add, remove, list_rules, bulk]
