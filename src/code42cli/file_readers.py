dc4ab43e0a1890aab74619361ca5ad680a90f793
Juliya Smith <juliya.smith@code42.com>
2020-05-19 20:58:50 +0000

    Move errors to error file

--- src/code42cli/file_readers.py
+++ src/code42cli/file_readers.py
@@ -1,15 +1,6 @@
 import csv
 
-
-class BadFileError(Exception):
-    def __init__(self, file_path, *args, **kwargs):
-        self.file_path = file_path
-        super(BadFileError, self).__init__()
-
-
-class EmptyFileError(BadFileError):
-    def __init__(self, file_path):
-        super(EmptyFileError, self).__init__(file_path, u"Given empty file {}.".format(file_path))
+from code42cli.errors import BadFileError
 
 
 class CliFileReader(object):
