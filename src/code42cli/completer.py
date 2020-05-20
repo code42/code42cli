481eda1570e90cddf8df2791f9d4ea1ac855f047
Juliya Smith <juliya.smith@code42.com>
2020-05-20 15:43:37 +0000

    Save

--- src/code42cli/completer.py
+++ src/code42cli/completer.py
@@ -1,3 +1,10 @@
 class Completer(object):
-    def __init__(self):
-        pass
+    def complete(self, cmdline, point=None):
+        if point is None:
+            point = len(cmdline)
+
+        args = cmdline[0:point].split()
+        current_arg = args[-1]
+        cmd_args = [w for w in args if not w.startswith("-")]
+        opts = [w for w in args if w.startswith("-")]
+        return ["departing-employee"]
