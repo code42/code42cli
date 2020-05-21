1e906d0c111a497a1b43d696161c13c2f98a8d9f
Juliya Smith <juliya.smith@code42.com>
2020-05-21 17:50:58 +0000

    Remove dedeclare

--- src/code42cli/cmds/profile.py
+++ src/code42cli/cmds/profile.py
@@ -20,21 +20,7 @@ class ProfileCommandController(CommandController):
     UPDATE = u"update"
     DELETE = u"delete"
     DELETE_ALL = u"delete-all"
-
-    @property
-    def names(self):
-        return [
-            self.SHOW,
-            self.LIST,
-            self.USE,
-            self.UPDATE,
-            self.RESET_PW,
-            self.CREATE,
-            self.UPDATE,
-            self.DELETE,
-            self.DELETE_ALL,
-        ]
-
+    
     def load_commands(self):
         """Sets up the `profile` subcommand with all of its subcommands."""
         usage_prefix = u"{} profile".format(MAIN_COMMAND)
