d49cfe76cbe264922cdf1b73779071d4acaaf214
Juliya Smith <juliya.smith@code42.com>
2020-05-27 13:33:41 +0000

    Refactor

--- src/code42cli/completer.py
+++ src/code42cli/completer.py
@@ -11,37 +11,44 @@ def _get_matches(current, options):
     return matches
 
 
+def _get_next_full_set_of_commands(cmd_loader, current):
+    cmd_loader = cmd_loader.subtrees[current]
+    return cmd_loader.names
+
+
 class Completer(object):
     def __init__(self, main_cmd_loader=None):
         self._main_cmd_loader = main_cmd_loader or MainSubcommandLoader(u"")
     
     def complete(self, cmdline, point=None):
         try:
-            if point is None:
-                point = len(cmdline)
+            point = point or len(cmdline)
             args = cmdline[0:point].split()
             if len(args) < 2:
-                if args[0] == MAIN_COMMAND:
-                    return self._main_cmd_loader.names
-                return []  # `code42` already completes w/o
+                # `code42` already completes w/o
+                return self._main_cmd_loader.names if args[0] == MAIN_COMMAND else []
     
-            cmd_loader = self._main_cmd_loader
             current = args[-1]
-            if len(args) > 2:
-                for arg in args[1:-1]:
-                    cmd_loader = cmd_loader.subtrees[arg]
-    
+            cmd_loader = self._search_trees(args)
             if not cmd_loader:
                 return []
             
+            # `current` is already complete
             options = cmd_loader.names
             if current in options:
-                cmd_loader = cmd_loader.subtrees[current]
-                return cmd_loader.names
+                return _get_next_full_set_of_commands(cmd_loader, current)
             
             return _get_matches(current, options) if options else []
         except:
             return []
+
+    def _search_trees(self, args):
+        # Find cmd_loader at lowest level from given args
+        cmd_loader = self._main_cmd_loader
+        if len(args) > 2:
+            for arg in args[1:-1]:
+                cmd_loader = cmd_loader.subtrees[arg]
+        return cmd_loader
 
 
 def complete(cmdline, point):
