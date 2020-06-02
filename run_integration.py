b754cc638c835457a12bf630db0b78e37a0bfd84
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-06-02 17:17:55 +0530

    Integration-test expects a profile set with password

--- run_integration.py
+++ run_integration.py
@@ -2,12 +2,5 @@ import sys
 import os
 
 if __name__ == "__main__":
-    try:
-        profile_password = os.environ["USER_PW"]
-        # TODO Validate profile is set
-    except KeyError:
-        print("Profile password not found in environment. Integration tests will be skipped.")
-        sys.exit(1)
-
     rc = os.system("pytest ./integration -v -rsxX -l --tb=short --strict")
     sys.exit(rc)
