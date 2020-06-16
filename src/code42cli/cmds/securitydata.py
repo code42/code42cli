c953b6e1695dee918cc49b9299a7ce5085d62d26
Tim Abramson <tim.abramson@code42.com>
2020-06-16 16:44:31 -0500

    add clear-checkpoint

--- src/code42cli/cmds/securitydata.py
+++ src/code42cli/cmds/securitydata.py
@@ -13,6 +13,10 @@ from code42cli.cmds.search_shared.extraction import (
     create_handlers,
     create_time_range_filter,
 )
+import code42cli.errors as errors
+from code42cli.logger import get_main_cli_logger
+
+logger = get_main_cli_logger()
 
 
 @click.group()
@@ -23,9 +27,9 @@ def security_data(sdk):
 
 def is_in_filter(filter_cls):
     def f(ctx, arg):
-        if arg is not None:
+        if arg:
             ctx.obj.search_filters.append(filter_cls.is_in(arg))
-            return arg
+        return arg
 
     return f
 
@@ -91,47 +95,58 @@ AdvancedQueryIncompatible = incompatible_with("advanced_query")
     callback=is_in_filter(ExposureType),
 )
 @click.option(
-    "--c42-username", callback=is_in_filter(DeviceUsername), cls=AdvancedQueryIncompatible
+    "--c42-username",
+    multiple=True,
+    callback=is_in_filter(DeviceUsername),
+    cls=AdvancedQueryIncompatible,
 )
-@click.option("--actor", callback=is_in_filter(Actor), cls=AdvancedQueryIncompatible)
-@click.option("--md5", callback=is_in_filter(MD5), cls=AdvancedQueryIncompatible)
-@click.option("--sha256", callback=is_in_filter(SHA256), cls=AdvancedQueryIncompatible)
-@click.option("--source", callback=is_in_filter(Source), cls=AdvancedQueryIncompatible)
-@click.option("--file-name", callback=is_in_filter(FileName), cls=AdvancedQueryIncompatible)
-@click.option("--file-path", callback=is_in_filter(FilePath), cls=AdvancedQueryIncompatible)
-@click.option("--process-owner", callback=is_in_filter(ProcessOwner), cls=AdvancedQueryIncompatible)
-@click.option("--tab-url", callback=is_in_filter(TabURL), cls=AdvancedQueryIncompatible)
+@click.option("--actor", multiple=True, callback=is_in_filter(Actor), cls=AdvancedQueryIncompatible)
+@click.option("--md5", multiple=True, callback=is_in_filter(MD5), cls=AdvancedQueryIncompatible)
+@click.option(
+    "--sha256", multiple=True, callback=is_in_filter(SHA256), cls=AdvancedQueryIncompatible
+)
+@click.option(
+    "--source", multiple=True, callback=is_in_filter(Source), cls=AdvancedQueryIncompatible
+)
+@click.option(
+    "--file-name", multiple=True, callback=is_in_filter(FileName), cls=AdvancedQueryIncompatible
+)
+@click.option(
+    "--file-path", multiple=True, callback=is_in_filter(FilePath), cls=AdvancedQueryIncompatible
+)
+@click.option(
+    "--process-owner",
+    multiple=True,
+    callback=is_in_filter(ProcessOwner),
+    cls=AdvancedQueryIncompatible,
+)
+@click.option(
+    "--tab-url", multiple=True, callback=is_in_filter(TabURL), cls=AdvancedQueryIncompatible
+)
 @click.option(
     "--include-non-exposure",
     is_flag=True,
     callback=exists_filter(ExposureType),
     cls=incompatible_with(["advanced_query", "type"]),
 )
-def _print(
-    sdk,
-    begin,
-    end,
-    format,
-    advanced_query,
-    type,
-    c42_username,
-    actor,
-    md5,
-    sha256,
-    source,
-    file_name,
-    file_path,
-    process_owner,
-    tab_url,
-    include_non_exposure,
-    incremental,
-):
+def _print(sdk, begin, end, format, advanced_query, incremental, **kwargs):
     """Print file events to stdout."""
-    filters = sdk.search_filters
     store = FileEventCursorStore(sdk.profile.name) if incremental else None
-    verify_begin_date_requirements(begin, incremental, store)
-    if begin or end:
-        filters.append(create_time_range_filter(EventTimestamp, begin, end))
     handlers = create_handlers(sdk, FileEventExtractor, format, store)
     extractor = FileEventExtractor(sdk, handlers)
-    extractor.extract(*filters)
+    if advanced_query:
+        extractor.extract_advanced(advanced_query)
+    else:
+        verify_begin_date_requirements(begin, incremental, store)
+        if begin or end:
+            sdk.search_filters.append(create_time_range_filter(EventTimestamp, begin, end))
+    print([str(f) for f in sdk.search_filters])
+    extractor.extract(*sdk.search_filters)
+    if handlers.TOTAL_EVENTS == 0 and not errors.ERRORED:
+        logger.print_info(u"No results found.")
+
+
+@security_data.command()
+@sdk_options
+def clear_checkpoint(sdk):
+    FileEventCursorStore(sdk.profile.name).replace_stored_cursor_timestamp(None)
