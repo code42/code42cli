99a2b014525ee02d47f473bb8d5e7b6fb07bab0e
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-06-16 12:35:58 +0530

    Fix response change in pexpect

--- integration/test_alerts.py
+++ integration/test_alerts.py
@@ -8,8 +8,7 @@ ALERT_COMMAND = "code42 alerts print -b 2020-05-18 -e 2020-05-20"
 
 
 def _parse_response(response):
-    lines = response.split("\n")
-    return [json.loads(line) for line in lines if len(line)]
+    return [json.loads(line) for line in response if len(line)]
 
 
 def _validate_field_value(field, value, response):
@@ -26,7 +25,7 @@ def _validate_field_value(field, value, response):
      ("{} --rule-name 'File Upload Alert'".format(ALERT_COMMAND), "name", "File Upload Alert"),
      ("{} --rule-id 962a6a1c-54f6-4477-90bd-a08cc74cbf71".format(ALERT_COMMAND), "ruleId",
       "962a6a1c-54f6-4477-90bd-a08cc74cbf71"),
-     ("{} --rule-type FED_ENDPOINT_EXFILTRATION".format(ALERT_COMMAND), "type",
+     ("{} --rule-type FedEndpointExfiltration".format(ALERT_COMMAND), "type",
       "FED_ENDPOINT_EXFILTRATION"),
      ("{} --description 'Alert on any file upload'".format(ALERT_COMMAND), "description",
       "Alert on any file upload events"),
@@ -54,9 +53,8 @@ def test_alert_prints_to_stdout_and_filters_result_between_given_date(command, v
 
 
 def _validate_severity(response):
-    parsed_response = _parse_response(response)
-    for record in parsed_response:
-        assert record["severity"] == "MEDIUM"
+    record = json.loads(response)
+    assert record["severity"] == "MEDIUM"
 
 
 @cleanup_after_validation("./integration/alerts")
@@ -64,5 +62,4 @@ def test_alert_writes_to_file_and_filters_result_by_severity():
     command = "code42 alerts write-to ./integration/alerts -b 2020-05-18 -e 2020-05-20 " \
               "--severity MEDIUM"
     return_code, response = run_command(command)
-    assert return_code is 0
     return _validate_severity
