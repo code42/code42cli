7ddc3f00a4db82ebc9edf0e92d6dc56f79009d4e
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-06-16 12:34:50 +0530

    Use pexpect for prompt

--- integration/__init__.py
+++ integration/__init__.py
@@ -1,18 +1,31 @@
-import subprocess
-import shlex
+import os
+import pexpect
+
+
+LINE_FEED = b'\r\n'
+PASSWORD_PROMPT = b'Password: '
+
+
+def command_response(lines):
+    return [line.decode('utf-8') for line in lines]
 
 
 def run_command(command):
-    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+
+    process = pexpect.spawn(command)
     response = ""
-    while True:
-        output = process.stdout.readline()
-        if output == b'' and process.poll() is not None:
-            break
-        else:
-            response += output.decode('utf-8')
-    rc = process.poll()
-    return rc, response
+    try:
+        expected = process.expect([PASSWORD_PROMPT, LINE_FEED])
+        if expected == 0:
+            process.sendline(os.environ["C42_PW"])
+            process.expect(LINE_FEED)
+        response = command_response(process.readlines())
+
+    except pexpect.EOF:
+        return 1, response
+    except pexpect.TIMEOUT:
+        return 1, response
+    return 0, response
 
 
 __all__ = [run_command]
--- run_integration.py
+++ run_integration.py
@@ -2,5 +2,10 @@ import sys
 import os
 
 if __name__ == "__main__":
-    rc = os.system("pytest ./integration -v -rsxX -l --tb=short --strict")
-    sys.exit(rc)
+    if sys.argv[1] and sys.argv[2]:
+        os.environ["C42_USER"] = sys.argv[1]
+        os.environ["C42_PW"] = sys.argv[2]
+        rc = os.system("pytest ./integration -v -rsxX -l --tb=short --strict")
+        sys.exit(rc)
+    else:
+        print("username and password were not supplied. Integration tests will be skipped.")
