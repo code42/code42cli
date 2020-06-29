4016849edcb1ad44f345e3565f3157e8e69cb8e6
tim.abramson <tim@abramson.dev>
2020-06-29 13:27:56 -0500

    specify click and colorama min versions

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     package_dir={"": "src"},
     python_requires=">3, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
     install_requires=[
-        "Click",
-        "colorama",
+        "click>=7.1.1",
+        "colorama>=0.4.3",
         "c42eventextractor==0.3.2",
         "keyring==18.0.1",
         "keyrings.alt==3.2.0",
