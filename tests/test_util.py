0720e289761e2f64ebc8541d32f983b4a7a171f7
tim.abramson <tim@abramson.dev>
2020-06-30 15:37:52 -0500

    fix util tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -9,11 +9,6 @@ TEST_HEADER = {u"key1": u"Column 1", u"key2": u"Column 10", u"key3": u"Column 10
 _NAMESPACE = "{}.util".format(PRODUCT_NAME)
 
 
-@pytest.fixture
-def mock_input(mocker):
-    return mocker.patch("{}.get_input".format(_NAMESPACE))
-
-
 def test_get_url_parts_when_given_host_and_port_returns_expected_parts():
     url_str = "www.example.com:123"
     parts = get_url_parts(url_str)
@@ -26,18 +21,18 @@ def test_get_url_parts_when_given_host_without_port_returns_expected_parts():
     assert parts == ("www.example.com", None)
 
 
-def test_does_user_agree_when_user_says_y_returns_true(mock_input):
-    mock_input.return_value = "y"
+def test_does_user_agree_when_user_says_y_returns_true(mocker):
+    mocker.patch("builtins.input", return_value="y")
     assert does_user_agree("Test Prompt")
 
 
-def test_does_user_agree_when_user_says_capital_y_returns_true(mock_input):
-    mock_input.return_value = "Y"
+def test_does_user_agree_when_user_says_capital_y_returns_true(mocker):
+    mocker.patch("builtins.input", return_value="Y")
     assert does_user_agree("Test Prompt")
 
 
-def test_does_user_agree_when_user_says_n_returns_false(mock_input):
-    mock_input.return_value = "n"
+def test_does_user_agree_when_user_says_n_returns_false(mocker):
+    mocker.patch("builtins.input", return_value="n")
     assert not does_user_agree("Test Prompt")
 
 
