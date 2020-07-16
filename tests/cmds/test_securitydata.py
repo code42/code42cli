6040c378e4bab2e447828d9b53616b94d41999ed
Juliya Smith <juliya.smith@code42.com>
2020-07-16 16:17:33 +0000

    Imply negation is test name / rm dup test name

--- tests/cmds/test_securitydata.py
+++ tests/cmds/test_securitydata.py
@@ -64,7 +64,7 @@ def test_search_when_is_advanced_query_uses_only_the_extract_advanced_method(
     assert file_event_extractor.extract_advanced.call_count == 1
 
 
-def test_search_when_is_advanced_query_uses_only_the_extract_advanced_method(
+def test_search_when_is_not_advanced_query_uses_only_the_extract_advanced_method(
     runner, cli_state, file_event_extractor
 ):
     result = runner.invoke(cli, ["security-data", "search", "--begin", "1d"], obj=cli_state)
