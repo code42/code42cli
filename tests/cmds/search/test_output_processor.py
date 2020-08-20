6ca78ea77ba964b8885d5d0c8ca3ae37e5cf2313
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-08-20 21:55:37 +0530

    fix test v3.5

--- tests/cmds/search/test_output_processor.py
+++ tests/cmds/search/test_output_processor.py
@@ -1,7 +1,13 @@
+from _collections import OrderedDict
+
 from code42cli.cmds.search.output_processor import print_events
 from code42cli.cmds.search.output_processor import send_events
 from code42cli.output_formats import to_csv
 
+
+header = OrderedDict()
+header["test"] = "Test"
+header["test2"] = "test2"
 
 EVENTS = [
     {"test": "value1", "test2": "value2"},
@@ -35,15 +41,19 @@ CSV_RESPONSE = "\r\n".join(
 
 
 def test_print_events_calls_echo_when_results_are_less_than_equal_to_10(mocker):
+    format_function = mocker.MagicMock()
+    format_function.return_value = CSV_RESPONSE
     mocked_echo = mocker.patch("click.echo")
-    print_events(to_csv, {"test": "Test", "test2": "Test2"})(EVENTS)
+    print_events(format_function, header)(EVENTS)
     mocked_echo.assert_called_once_with(CSV_RESPONSE)
 
 
 def test_print_events_calls_echo_via_pager_when_results_are_greater_than_10(mocker):
+    format_function = mocker.MagicMock()
+    format_function.return_value = CSV_RESPONSE
     mocked_echo_pager = mocker.patch("click.echo_via_pager")
     EVENTS.append({"test": "valueX", "test2": "value2"})
-    print_events(to_csv, {"test": "Test", "test2": "Test2"})(EVENTS)
+    print_events(format_function, header)(EVENTS)
     assert mocked_echo_pager.call_count == 1
 
 
