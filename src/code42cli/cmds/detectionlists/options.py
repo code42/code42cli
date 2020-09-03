7d50158fa7e4cb26b0edbc661aaa470269e95bda
Juliya Smith <juliya.smith@code42.com>
2020-09-03 18:05:44 +0000

    Add warning

--- src/code42cli/cmds/detectionlists/options.py
+++ src/code42cli/cmds/detectionlists/options.py
@@ -5,6 +5,7 @@ cloud_alias_option = click.option(
     "--cloud-alias",
     help="If the employee has an email alias other than their Code42 username "
     "that they use for cloud services such as Google Drive, OneDrive, or Box, "
-    "add and monitor the alias.",
+    "add and monitor the alias. WARNING: Adding a cloud alias will override the "
+    "existing user cloud alias if they already have one.",
 )
 notes_option = click.option("--notes", help="Optional notes about the employee.")
