06a06df75045ed89126e24cc83c9b1f8e9730901
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-10-09 12:05:45 +0530

    Fix style

--- integration/test_alerts.py
+++ integration/test_alerts.py
@@ -1,8 +1,7 @@
-import json
-
-import pytest
 from datetime import datetime
 from datetime import timedelta
+
+import pytest
 from integration import run_command
 
 
@@ -11,7 +10,8 @@ end_date = datetime.utcnow() - timedelta(days=10)
 begin_date_str = begin_date.strftime("%Y-%m-%d")
 end_date_str = end_date.strftime("%Y-%m-%d")
 
-ALERT_COMMAND = "code42 alerts search -b {0} -e {1}".format(begin_date_str, end_date_str)
+ALERT_COMMAND = "code42 alerts search -b {} -e {}".format(begin_date_str, end_date_str)
+
 
 @pytest.mark.parametrize(
     "command",
@@ -19,25 +19,13 @@ ALERT_COMMAND = "code42 alerts search -b {0} -e {1}".format(begin_date_str, end_
         ("{}".format(ALERT_COMMAND)),
         ("{} --state OPEN".format(ALERT_COMMAND)),
         ("{} --state RESOLVED".format(ALERT_COMMAND)),
-        (
-            "{} --actor user@code42.com".format(ALERT_COMMAND)
-        ),
-        (
-            "{} --rule-name 'File Upload Alert'".format(ALERT_COMMAND)
-        ),
-        (
-            "{} --rule-id 962a6a1c-54f6-4477-90bd-a08cc74cbf71".format(ALERT_COMMAND)
-        ),
-        (
-            "{} --rule-type FedEndpointExfiltration".format(ALERT_COMMAND)
-        ),
-        (
-            "{} --description 'Alert on any file upload'".format(ALERT_COMMAND)
-        ),
+        ("{} --actor user@code42.com".format(ALERT_COMMAND)),
+        ("{} --rule-name 'File Upload Alert'".format(ALERT_COMMAND)),
+        ("{} --rule-id 962a6a1c-54f6-4477-90bd-a08cc74cbf71".format(ALERT_COMMAND)),
+        ("{} --rule-type FedEndpointExfiltration".format(ALERT_COMMAND)),
+        ("{} --description 'Alert on any file upload'".format(ALERT_COMMAND)),
     ],
 )
-def test_alert_returns_success_return_code(
-    command
-):
+def test_alert_returns_success_return_code(command):
     return_code, response = run_command(command)
     assert return_code == 0
