1ce7c49f4c6c889966e1ac356492d668c03bbceb
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2020-10-12 16:45:02 -0500

    added get_info, cold storage and device name updating for deactivate, and some additional tests for deactivate

--- src/code42cli/cmds/devices.py
+++ src/code42cli/cmds/devices.py
@@ -1,45 +1,136 @@
-import click
-from click import echo
+from collections import OrderedDict
+from datetime import date
 
-from code42cli.bulk import generate_template_cmd_factory
-from code42cli.bulk import run_bulk_process
+import click
+from py42 import exceptions
+
 from code42cli.click_ext.groups import OrderedGroup
+from code42cli.errors import Code42CLIError
 from code42cli.options import format_option
 from code42cli.options import sdk_options
-from code42cli.output_formats import OutputFormat
 from code42cli.output_formats import OutputFormatter
-from code42cli.util import format_string_list_to_columns
-from code42cli.errors import Code42CLIError
-
-from py42 import exceptions
 
 
 @click.group(cls=OrderedGroup)
 @sdk_options(hidden=True)
 def devices(state):
-	"""For managing computers within Code42."""
-	pass
+    """For managing computers within Code42."""
+    pass
+
 
 device_id_option = click.option(
-	'--device-id',
-	required=True,
-	type=str,
-	help='The device ID for the device on which you wish to act.'
+    "--device-id",
+    required=True,
+    type=str,
+    help="The device ID for the device on which you wish to act.",
 )
+
+change_device_name_option = click.option(
+    "--change-device-name",
+    required=False,
+    is_flag=True,
+    default=False,
+    help='Include to prepend "deactivated_" and today\'s date to the name of any deactivated devices.',
+)
+
+purge_date_option = click.option(
+    "--purge-date",
+    required=False,
+    type=str,
+    default=None,
+    help="The date on which the archive should be purged from cold storage in yyyy-MM-dd format. If not provided, the date will be set according to the appropriate org settings.",
+)
+
 
 @devices.command()
 @device_id_option
+@change_device_name_option
+@purge_date_option
 @sdk_options()
-def deactivate(state,device_id):
-	"""Deactivate a device within Code42"""
-	_deactivate_device(state.sdk, device_id)
+def deactivate(state, device_id, change_device_name, purge_date):
+    """Deactivate a device within Code42"""
+    _deactivate_device(state.sdk, device_id, change_device_name, purge_date)
 
-def _deactivate_device(sdk, device_id):
-	try:
-		sdk.devices.deactivate(device_id)
-	except exceptions.Py42BadRequestError:
-		raise Code42CLIError(u"The device {} is in legal hold.".format(device_id))
-	except exceptions.Py42NotFoundError:
-		raise Code42CLIError(u"The device {} was not found.".format(device_id))
-	except exceptions.Py42ForbiddenError:
-		raise Code42CLIError(u"Unable to deactivate {}.".format(device_id))
+
+def _deactivate_device(sdk, device_id, change_device_name, purge_date):
+    try:
+        sdk.devices.deactivate(device_id)
+    except exceptions.Py42BadRequestError:
+        raise Code42CLIError("The device {} is in legal hold.".format(device_id))
+    except exceptions.Py42NotFoundError:
+        raise Code42CLIError("The device {} was not found.".format(device_id))
+    except exceptions.Py42ForbiddenError:
+        raise Code42CLIError("Unable to deactivate {}.".format(device_id))
+    if purge_date or change_device_name:
+        device = sdk.devices.get_by_id(device_id)
+        guid = device.data["guid"]
+        name = device.data["name"]
+    if purge_date:
+        _update_cold_storage_purge_date(sdk, guid, purge_date)
+    if change_device_name and not name.startswith("deactivated_"):
+        _change_device_name(
+            sdk, guid, "deactivated_" + date.today().strftime("%Y-%m-%d") + "_" + name
+        )
+
+
+def _update_cold_storage_purge_date(sdk, guid, purge_date):
+    archives_response = sdk.archive.get_all_by_device_guid(guid)
+    archive_guid_list = [
+        archive["archiveGuid"]
+        for page in archives_response
+        for archive in page["archives"]
+    ]
+    for archive_guid in archive_guid_list:
+        sdk.archive.update_cold_storage_purge_date(archive_guid, purge_date)
+
+
+def _change_device_name(sdk, guid, name):
+    device_settings = sdk.devices.get_settings(guid)
+    device_settings.name = name
+    sdk.devices.update_settings(device_settings)
+
+
+_DEVICE_INFO_KEYS_MAP = OrderedDict()
+_DEVICE_INFO_KEYS_MAP["name"] = "Name"
+_DEVICE_INFO_KEYS_MAP["osHostname"] = "Hostname"
+_DEVICE_INFO_KEYS_MAP["guid"] = "GUID"
+_DEVICE_INFO_KEYS_MAP["status"] = "Status"
+_DEVICE_INFO_KEYS_MAP["archiveBytes"] = "Largest Archive Size in Bytes"
+_DEVICE_INFO_KEYS_MAP["lastConnected"] = "Last Connected Date"
+_DEVICE_INFO_KEYS_MAP["lastBackup"] = "Last Sent Backup Data Date"
+_DEVICE_INFO_KEYS_MAP["lastCompletedBackup"] = "Last Completed Backup Date"
+_DEVICE_INFO_KEYS_MAP["productVersion"] = "Code42 Version"
+_DEVICE_INFO_KEYS_MAP["osName"] = "Operating System"
+_DEVICE_INFO_KEYS_MAP["osVersion"] = "Operating System Version"
+
+
+@devices.command()
+@device_id_option
+@format_option
+@sdk_options()
+def get_info(state, device_id, format=None):
+    """Print device info."""
+    formatter = OutputFormatter(format, _DEVICE_INFO_KEYS_MAP)
+    device_info = _get_device_info(state.sdk, device_id)
+    if device_info:
+        formatter.echo_formatted_list([device_info])
+
+
+def _get_device_info(sdk, device_id):
+    device = sdk.devices.get_by_id(device_id, include_backup_usage=True).data
+    device["archiveBytes"] = max(
+        [
+            backupDestination["archiveBytes"]
+            for backupDestination in device["backupUsage"]
+        ]
+    )
+    device["lastBackup"] = max(
+        [backupDestination["lastBackup"] for backupDestination in device["backupUsage"]]
+    )
+    device["lastCompletedBackup"] = max(
+        [
+            backupDestination["lastCompletedBackup"]
+            for backupDestination in device["backupUsage"]
+        ]
+    )
+    return device
--- tests/cmds/test_devices.py
+++ tests/cmds/test_devices.py
@@ -1,5 +1,9 @@
+from datetime import date
+
 import pytest
-from py42.exceptions import Py42BadRequestError, Py42ForbiddenError, Py42NotFoundError
+from py42.exceptions import Py42BadRequestError
+from py42.exceptions import Py42ForbiddenError
+from py42.exceptions import Py42NotFoundError
 from py42.response import Py42Response
 from requests import HTTPError
 from requests import Response
@@ -7,6 +11,33 @@ from requests import Response
 from code42cli.main import cli
 
 TEST_DEVICE_ID = "12345"
+TEST_ARCHIVE_GUID = "954143426849296547"
+TEST_PURGE_DATE = "2020-10-12"
+TEST_ARCHIVES_RESPONSE = [
+    {
+        "archives": [
+            {
+                "archiveGuid": "954143426849296547",
+                "userId": None,
+                "userUid": None,
+                "archiveBytes": 1745757673,
+                "targetGuid": "632540230984925185",
+                "lastCompletedBackup": "2020-10-12T20:17:52.084Z",
+                "isColdStorage": False,
+                "lastMaintained": "2020-10-10T19:31:05.811Z",
+                "maintenanceDuration": 455,
+                "compactBytesRemoved": 0,
+                "storePointId": 1000,
+                "selectedBytes": 1658317953,
+                "selectedFiles": 596,
+                "todoBytes": 0,
+                "format": "ARCHIVE_V1",
+            }
+        ]
+    }
+]
+TEST_DEVICE_RESPONSE = """{"data":{"computerId":139527,"name":"testname","osHostname":"testhostname","guid":"954143368874689941","type":"COMPUTER","status":"Active","active":true,"blocked":false,"alertState":0,"alertStates":["OK"],"userId":203988,"userUid":"938960273869958201","orgId":3099,"orgUid":"915323705751579872","computerExtRef":null,"notes":null,"parentComputerId":null,"parentComputerGuid":null,"lastConnected":"2020-10-12T16:55:40.632Z","osName":"win","osVersion":"10.0.18362","osArch":"amd64","address":"172.16.208.140:4242","remoteAddress":"72.50.201.186","javaVersion":"11.0.4","modelInfo":null,"timeZone":"America/Chicago","version":1525200006822,"productVersion":"8.2.2","buildVersion":26,"creationDate":"2020-05-14T13:03:20.302Z","modificationDate":"2020-10-12T16:55:40.632Z","loginDate":"2020-10-12T12:54:45.132Z","service":"CrashPlan"}}"""
+
 
 def _create_py42_response(mocker, text):
     response = mocker.MagicMock(spec=Response)
@@ -15,78 +46,146 @@ def _create_py42_response(mocker, text):
     response.status_code = 200
     return Py42Response(response)
 
-@pytest.fixture
-def deactivate_response(mocker):
-	return _create_py42_response(mocker, "")
 
 @pytest.fixture
-def deactivate_device_success(cli_state):
-	cli_state.sdk.devices.deactivate.return_value = deactivate_response
+def mock_device_settings(mocker):
+    device_settings = mocker.MagicMock()
+    device_settings.name = "testname"
+    return device_settings
+
+
+@pytest.fixture
+def mock_today(mocker):
+    mock_date = mocker.MagicMock(spec=date)
+    mock_date.today.return_value = date.fromtimestamp(1602538925)  # 2020-10-12
+    return mock_date
+
+
+@pytest.fixture
+def deactivate_response(mocker):
+    return _create_py42_response(mocker, "")
+
+
+@pytest.fixture
+def device_info_response(mocker):
+    return _create_py42_response(mocker, TEST_DEVICE_RESPONSE)
+
+
+@pytest.fixture
+def archives_list_generator(mocker):
+    yield TEST_ARCHIVES_RESPONSE
+
+
+@pytest.fixture
+def device_info_success(cli_state, device_info_response):
+    cli_state.sdk.devices.get_by_id.return_value = device_info_response
+
+
+@pytest.fixture
+def archives_list_success(cli_state, archives_list_generator):
+    cli_state.sdk.archive.get_all_by_device_guid.return_value = archives_list_generator
+
+
+@pytest.fixture
+def deactivate_device_success(cli_state, deactivate_response):
+    cli_state.sdk.devices.deactivate.return_value = deactivate_response
+
 
 @pytest.fixture
 def deactivate_device_not_found_failure(cli_state):
-	cli_state.sdk.devices.deactivate.side_effect = Py42NotFoundError(HTTPError())
+    cli_state.sdk.devices.deactivate.side_effect = Py42NotFoundError(HTTPError())
+
 
 @pytest.fixture
 def deactivate_device_in_legal_hold_failure(cli_state):
-	cli_state.sdk.devices.deactivate.side_effect = Py42BadRequestError(HTTPError())
+    cli_state.sdk.devices.deactivate.side_effect = Py42BadRequestError(HTTPError())
+
 
 @pytest.fixture
 def deactivate_device_not_allowed_failure(cli_state):
-	cli_state.sdk.devices.deactivate.side_effect = Py42ForbiddenError(HTTPError())
+    cli_state.sdk.devices.deactivate.side_effect = Py42ForbiddenError(HTTPError())
+
 
 def test_deactivate_deactivates_device(runner, cli_state, deactivate_device_success):
-	result = runner.invoke(
-		cli,
-		[
-			"devices",
-			"deactivate",
-			"--device-id",
-			TEST_DEVICE_ID
-		],
-		obj=cli_state
-	)
-	cli_state.sdk.devices.deactivate.assert_called_once_with(TEST_DEVICE_ID)
+    runner.invoke(
+        cli, ["devices", "deactivate", "--device-id", TEST_DEVICE_ID], obj=cli_state
+    )
+    cli_state.sdk.devices.deactivate.assert_called_once_with(TEST_DEVICE_ID)
 
 
-def test_deactivate_fails_if_device_does_not_exist(runner, cli_state, deactivate_device_not_found_failure):
-	result = runner.invoke(
-		cli,
-		[
-			"devices",
-			"deactivate",
-			"--device-id",
-			TEST_DEVICE_ID
-		],
-		obj=cli_state
-	)
-	assert result.exit_code == 1
-	assert ("The device {} was not found.".format(TEST_DEVICE_ID))
+def test_deactivate_updates_purge_date(
+    runner,
+    cli_state,
+    deactivate_device_success,
+    device_info_success,
+    archives_list_success,
+):
+    runner.invoke(
+        cli,
+        [
+            "devices",
+            "deactivate",
+            "--device-id",
+            TEST_DEVICE_ID,
+            "--purge-date",
+            TEST_PURGE_DATE,
+        ],
+        obj=cli_state,
+    )
+    cli_state.sdk.archive.update_cold_storage_purge_date.assert_called_once_with(
+        TEST_ARCHIVE_GUID, TEST_PURGE_DATE
+    )
 
-def test_deactivate_fails_if_device_is_on_legal_hold(runner, cli_state, deactivate_device_in_legal_hold_failure):
-	result = runner.invoke(
-		cli,
-		[
-			"devices",
-			"deactivate",
-			"--device-id",
-			TEST_DEVICE_ID
-		],
-		obj=cli_state
-	)
-	assert result.exit_code == 1
-	assert ("The device {} is in legal hold.".format(TEST_DEVICE_ID))
 
-def test_deactivate_fails_if_device_deactivation_forbidden(runner, cli_state, deactivate_device_not_allowed_failure):
-	result = runner.invoke(
-		cli,
-		[
-			"devices",
-			"deactivate",
-			"--device-id",
-			TEST_DEVICE_ID
-		],
-		obj=cli_state
-	)
-	assert result.exit_code == 1
-	assert ("Unable to deactivate {}.".format(TEST_DEVICE_ID))
+def test_deactivate_changes_device_name(
+    runner,
+    cli_state,
+    deactivate_device_success,
+    device_info_success,
+    mock_device_settings,
+    mock_today,
+):
+    cli_state.sdk.devices.get_settings.return_value = mock_device_settings
+    runner.invoke(
+        cli,
+        [
+            "devices",
+            "deactivate",
+            "--device-id",
+            TEST_DEVICE_ID,
+            "--change-device-name",
+        ],
+        obj=cli_state,
+    )
+    assert mock_device_settings.name == "deactivated_2020-10-12_testname"
+    cli_state.sdk.devices.update_settings.assert_called_once_with(mock_device_settings)
+
+
+def test_deactivate_fails_if_device_does_not_exist(
+    runner, cli_state, deactivate_device_not_found_failure
+):
+    result = runner.invoke(
+        cli, ["devices", "deactivate", "--device-id", TEST_DEVICE_ID], obj=cli_state
+    )
+    assert result.exit_code == 1
+    assert "The device {} was not found.".format(TEST_DEVICE_ID)
+
+
+def test_deactivate_fails_if_device_is_on_legal_hold(
+    runner, cli_state, deactivate_device_in_legal_hold_failure
+):
+    result = runner.invoke(
+        cli, ["devices", "deactivate", "--device-id", TEST_DEVICE_ID], obj=cli_state
+    )
+    assert result.exit_code == 1
+    assert "The device {} is in legal hold.".format(TEST_DEVICE_ID)
+
+
+def test_deactivate_fails_if_device_deactivation_forbidden(
+    runner, cli_state, deactivate_device_not_allowed_failure
+):
+    result = runner.invoke(
+        cli, ["devices", "deactivate", "--device-id", TEST_DEVICE_ID], obj=cli_state
+    )
+    assert result.exit_code == 1
+    assert "Unable to deactivate {}.".format(TEST_DEVICE_ID)
