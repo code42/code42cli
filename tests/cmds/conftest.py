4d237e843e1c30583641076bed5e8481c5e19046
Juliya Smith <juliya.smith@code42.com>
2020-12-15 21:58:24 +0000

    Simplify test generator

--- tests/cmds/conftest.py
+++ tests/cmds/conftest.py
@@ -116,8 +116,6 @@ def get_generator_for_get_all(mocker, mock_return_items):
     def gen(*args, **kwargs):
         response = mocker.MagicMock(spec=Request)
         response.text = """{{"items": [{0}]}}""".format(mock_return_items)
-        pages = [response]
-        for page in pages:
-            yield Py42Response(page)
+        yield Py42Response(response)
 
     return gen
