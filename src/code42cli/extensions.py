c4c397660a97e64f3b4bd5c0c5149861b2d38338
Tim Abramson <tim.abramson@code42.com>
2021-02-04 07:19:09 -0600

    alias sdk_options in extensions.py

--- src/code42cli/extensions.py
+++ src/code42cli/extensions.py
@@ -1,5 +1,6 @@
 from code42cli.click_ext.groups import ExtensionGroup
 from code42cli.main import CONTEXT_SETTINGS
+from code42cli.options import sdk_options  # alias for convenience in extension scripts
 
 script = ExtensionGroup(
     context_settings=CONTEXT_SETTINGS, invoke_without_command=True, no_args_is_help=True
