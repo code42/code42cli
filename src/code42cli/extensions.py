3a2e5e55f56aabc18887ed53b07ec9530880dbc9
Tim Abramson <tim.abramson@code42.com>
2021-02-04 08:36:46 -0600

    style

--- src/code42cli/extensions.py
+++ src/code42cli/extensions.py
@@ -1,7 +1,5 @@
 from code42cli.click_ext.groups import ExtensionGroup
 from code42cli.main import CONTEXT_SETTINGS
-
-# alias for convenience in extension scripts
 from code42cli.options import sdk_options  # noqa: F401
 
 script = ExtensionGroup(context_settings=CONTEXT_SETTINGS)
