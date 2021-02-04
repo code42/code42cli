8a6f9ea0d4398185cb936b6fccb10a07223b9883
Tim Abramson <tim.abramson@code42.com>
2021-02-04 06:36:25 -0600

    adjust arg swap given we know it's arg[0]

--- src/code42cli/click_ext/groups.py
+++ src/code42cli/click_ext/groups.py
@@ -26,8 +26,7 @@ _DIFFLIB_CUT_OFF = 0.6
 class InterpreterGroup(click.Group):
     def parse_args(self, ctx, args):
         if args[0] == "--python":
-            args.remove("--python")
-            args.insert(0, sys.executable)
+            args[0] = sys.executable
             if platform.system() == "Windows":
                 cmd = " ".join(args)
             else:
