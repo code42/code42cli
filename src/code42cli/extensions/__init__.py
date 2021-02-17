1b7dfaa91b9ad05e3e78a9b5bd2b5b5c5452138c
Tim Abramson <tim.abramson@code42.com>
2021-02-17 12:35:38 -0600

    add unused import ignore to setup.cfg, document import to prevent future removal

--- src/code42cli/extensions/__init__.py
+++ src/code42cli/extensions/__init__.py
@@ -1,5 +1,5 @@
 from code42cli.click_ext.groups import ExtensionGroup
 from code42cli.main import CONTEXT_SETTINGS
-from code42cli.options import sdk_options
+from code42cli.options import sdk_options  # convenience import for extension scripts
 
 script = ExtensionGroup(context_settings=CONTEXT_SETTINGS)
