908d1f034d6a53a24b5170b956eefb932a852245
Tim Abramson <tim.abramson@code42.com>
2021-02-17 12:23:40 -0600

    make extensions a module

--- src/code42cli/extensions/__init__.py
+++ src/code42cli/extensions/__init__.py
@@ -1,5 +1,5 @@
 from code42cli.click_ext.groups import ExtensionGroup
 from code42cli.main import CONTEXT_SETTINGS
-from code42cli.options import sdk_options  # noqa: F401
+from code42cli.options import sdk_options
 
 script = ExtensionGroup(context_settings=CONTEXT_SETTINGS)
