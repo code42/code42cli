3c976a7027b01f2c74464cfd04739c8ad25c3fbb
Juliya Smith <juliya.smith@code42.com>
2021-04-21 15:47:53 +0000

    conform help gen messag

--- src/code42cli/cmds/alerts.py
+++ src/code42cli/cmds/alerts.py
@@ -322,7 +322,7 @@ UPDATE_ALERT_CSV_HEADERS = ["id", "state", "note"]
 update_alerts_generate_template = generate_template_cmd_factory(
     group_name="alerts",
     commands_dict={"update": UPDATE_ALERT_CSV_HEADERS},
-    help_message="Generate the CSV template needed for bulk alert operation.",
+    help_message="Generate the CSV template needed for bulk alert commands.",
 )
 bulk.add_command(update_alerts_generate_template)
 
