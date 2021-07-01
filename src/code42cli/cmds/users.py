efb08732660710bfdab1673a501103c59249c89f
Juliya Smith <juliya.smith@code42.com>
2021-07-01 10:48:40 -0500

    inc csv format in help

--- src/code42cli/cmds/users.py
+++ src/code42cli/cmds/users.py
@@ -184,7 +184,8 @@ bulk.add_command(users_generate_template)
 @format_option
 @sdk_options()
 def bulk_update(state, csv_rows, format):
-    """Update a list of users from the provided CSV."""
+    """Update a list of users from the provided CSV 
+    in format: user_id,username,email,password,first_name,last_name,notes,archive_size_quota."""
     csv_rows[0]["updated"] = "False"
     formatter = OutputFormatter(format, {key: key for key in csv_rows[0].keys()})
     stats = create_worker_stats(len(csv_rows))
@@ -221,7 +222,8 @@ def bulk_update(state, csv_rows, format):
 @format_option
 @sdk_options()
 def bulk_move(state, csv_rows, format):
-    """Change the organization of the list of users from the provided CSV."""
+    """Change the organization of the list of users from the provided CSV in format: 
+    username,org_id."""
     csv_rows[0]["moved"] = "False"
     formatter = OutputFormatter(format, {key: key for key in csv_rows[0].keys()})
     stats = create_worker_stats(len(csv_rows))
