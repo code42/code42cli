4fb2ec4a94eafa17d92443b931faa6c8c01b87c8
Juliya Smith <juliya.smith@code42.com>
2021-07-02 07:29:34 -0500

    adjust comment

--- src/code42cli/cmds/users.py
+++ src/code42cli/cmds/users.py
@@ -189,7 +189,11 @@ bulk.add_command(users_generate_template)
 @sdk_options()
 def bulk_update(state, csv_rows, format):
     """Update a list of users from the provided CSV."""
-    sdk = state.sdk  # Initialize the SDK only once (for reasons like 2FA).
+
+    # Initialize the SDK before starting any bulk processes
+    # to prevent multiple instances and having to enter 2fa multiple times.
+    sdk = state.sdk
+    
     csv_rows[0]["updated"] = "False"
     formatter = OutputFormatter(format, {key: key for key in csv_rows[0].keys()})
     stats = create_worker_stats(len(csv_rows))
@@ -225,7 +229,11 @@ def bulk_update(state, csv_rows, format):
 @sdk_options()
 def bulk_move(state, csv_rows, format):
     """Change the organization of the list of users from the provided CSV."""
-    sdk = state.sdk  # Initialize the SDK only once (for reasons like 2FA).
+    
+    # Initialize the SDK before starting any bulk processes
+    # to prevent multiple instances and having to enter 2fa multiple times.
+    sdk = state.sdk
+
     csv_rows[0]["moved"] = "False"
     formatter = OutputFormatter(format, {key: key for key in csv_rows[0].keys()})
     stats = create_worker_stats(len(csv_rows))
