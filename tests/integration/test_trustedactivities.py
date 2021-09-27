e2b1e81dc88c9d5433a727b84c7e07cbd2c07a46
Tora Kozic <tora.kozic@code42.com>
2021-09-27 11:25:18 -0500

    remove trusted-activities show integration test

--- tests/integration/test_trustedactivities.py
+++ tests/integration/test_trustedactivities.py
@@ -10,10 +10,3 @@ def test_trusted_activities_list_command_returns_success_return_code(
     command = "trusted-activities list"
     assert_test_is_successful(runner, append_profile(command))
 
-
-@pytest.mark.integration
-def test_trusted_activities_show_command_returns_success_return_code(
-    runner, integration_test_profile
-):
-    command = "trusted-activities show 1"
-    assert_test_is_successful(runner, append_profile(command))
