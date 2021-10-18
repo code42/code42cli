9c08364279c2fe5ab946501f11978cbf9a8af196
Tora Kozic <tora.kozic@code42.com>
2021-10-18 09:39:14 -0500

    fix

--- src/code42cli/cmds/users.py
+++ src/code42cli/cmds/users.py
@@ -568,7 +568,7 @@ def _get_legacy_user_id(sdk, username):
     return user_id
 
 
-@functools.lru_cache
+@functools.lru_cache()
 def _get_role_id(sdk, role_name):
     try:
         roles_dataframe = DataFrame.from_records(
