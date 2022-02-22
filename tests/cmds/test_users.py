89736bd4a917df18b4ac608fcd908010b314e4e7
Tora Kozic <tora.kozic@code42.com>
2022-02-22 10:29:11 -0600

    style

--- tests/cmds/test_users.py
+++ tests/cmds/test_users.py
@@ -1434,10 +1434,12 @@ def test_list_aliases_calls_get_user_with_expected_parameters(runner, cli_state)
     cli_state.sdk.detectionlists.get_user.assert_called_once_with("alias@example")
 
 
-def test_list_aliases_prints_no_aliases_found_when_empty_list(runner, cli_state, mocker):
-    cli_state.sdk.detectionlists.get_user.return_value = create_mock_response(mocker, data={
-        "cloudUsernames": []
-    })
+def test_list_aliases_prints_no_aliases_found_when_empty_list(
+    runner, cli_state, mocker
+):
+    cli_state.sdk.detectionlists.get_user.return_value = create_mock_response(
+        mocker, data={"cloudUsernames": []}
+    )
     username = "alias@example"
     command = ["users", "list-aliases", username]
     result = runner.invoke(cli, command, obj=cli_state)
