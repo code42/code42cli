5e767438fe7a0de050ca48b8dabf9dbd97819c7a
tim.abramson <tim.abramson@code42.com>
2022-04-27 13:41:15 -0500

    unused import

--- src/code42cli/cmds/watchlists.py
+++ src/code42cli/cmds/watchlists.py
@@ -11,7 +11,6 @@ from code42cli.click_ext.groups import OrderedGroup
 from code42cli.click_ext.options import incompatible_with
 from code42cli.click_ext.types import AutoDecodedFile
 from code42cli.errors import Code42CLIError
-from code42cli.file_readers import read_csv_arg
 from code42cli.options import format_option
 from code42cli.options import sdk_options
 from code42cli.output_formats import DataFrameOutputFormatter
